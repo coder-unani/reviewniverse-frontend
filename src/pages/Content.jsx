ec231356e93fe69c7ae94de17d1e18ee7de77529
hwakk <97441379+hwakk@users.noreply.github.com>
2024-06-20 00:00:52 +0900

    [feat] content api 연동

--- src/pages/Content.jsx
+++ src/pages/Content.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import HttpClient from "/src/utils/HttpClient";
+import { cLog, cError } from "/src/utils/test";
 
 const Content = () => {
-  return <div>Content</div>;
+  const { contentId } = useParams();
+  const [video, setVideo] = useState({});
+
+  useEffect(() => {
+    cLog(video);
+  }, [video]);
+
+  useEffect(() => {
+    try {
+      const client = new HttpClient();
+      client
+        .get(`https://comet.orbitcode.kr/v1/contents/videos/${contentId}`)
+        .then((res) => {
+          if (res.status === 200 && res.code === "VIDEO_READ_SUCC") {
+            setVideo(res.data.data);
+          } else {
+            cLog("콘텐츠를 불러오는데 실패하였습니다.");
+            return;
+          }
+        });
+    } catch (error) {
+      cError(error);
+    }
+  }, []);
+
+  return <div>Content {contentId}</div>;
 };
 
 export default Content;
