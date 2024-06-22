d2dc6585f65ba0d9be44964e890cf51f32466718
hwakk <97441379+hwakk@users.noreply.github.com>
2024-06-22 21:01:16 +0900

    [style] footer 레이아웃 수정

--- src/layouts/default/footer/index.jsx
+++ src/layouts/default/footer/index.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer className="footer">footer</footer>;
+  const year = new Date().getFullYear();
+  return (
+    <footer className="footer-wrapper">
+      <div className="footer">
+        <div className="left">
+          <span>© {year}. Orbitcode Co. All rights reserved.</span>
+        </div>
+        <div className="right"></div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
