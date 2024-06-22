f78cd0c3594c1e94454d8b6b6e159aef196a1014
hwakk <97441379+hwakk@users.noreply.github.com>
2024-06-22 21:00:53 +0900

    [feat] 랭킹 컴포넌트 수정 및 추가

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MovieRanking from "../components/MovieRanking";
+import MainRanking from "/src/components/MainRanking";
 
 const Movie = () => {
   return (
     <main className="main">
-      <MovieRanking title={"🎬 박스오피스 순위"} />
+      {/* <MovieRanking title={"🎬 박스오피스 순위"} /> */}
+      <MainRanking type={10} />
     </main>
   );
 };
--- src/pages/Series.jsx
+++ src/pages/Series.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MovieRanking from "../components/MovieRanking";
+import MainRanking from "/src/components/MainRanking";
 
 const Series = () => {
   return (
     <main className="main">
-      <MovieRanking title={"넷플릭스 순위"} />
+      {/* <MovieRanking title={"넷플릭스 순위"} /> */}
+      <MainRanking type={11} />
     </main>
   );
 };
