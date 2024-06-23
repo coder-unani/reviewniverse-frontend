1ccbc0c2f9077d23856edd027c25da17464f67a7
hwakk <97441379+hwakk@users.noreply.github.com>
2024-06-23 23:06:02 +0900

    [feat] 갤러리 이미지 클릭시 이미지 확대 모달창 구현

---
+++ src/components/Modal/PhotoModal.jsx
@@ -0,0 +1,23 @@
+import React, { useRef } from "react";
+import Modal from "/src/components/Modal/Modal";
+import "/src/styles/PhotoModal.css";
+
+const PhotoModal = (props) => {
+  const { url, onClose } = props;
+  const modalRef = useRef();
+
+  // 이미지 바깥 영역 클릭시 모달 닫기
+  const handleModalClose = (e) => {
+    if (e.target === modalRef.current) onClose();
+  };
+
+  return (
+    <Modal>
+      <div className="photo-modal" ref={modalRef} onClick={handleModalClose}>
+        <img src={url} alt="스틸컷" />
+      </div>
+    </Modal>
+  );
+};
+
+export default PhotoModal;
