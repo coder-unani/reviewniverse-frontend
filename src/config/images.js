579f9a9aacc08c545b13b6461cb975e288cad95d
hwakk <97441379+hwakk@users.noreply.github.com>
2024-06-26 04:13:00 +0900

    [feat] 기본 이미지 설정 추가

---
+++ src/config/images.js
@@ -0,0 +1,7 @@
+export const DEFAULT_IMAGES = {
+  logo: "/src/assets/logo.svg",
+  logoWhite: "/src/assets/logo-w.svg",
+  noProfile: "/src/assets/no-profile.png",
+  noActor: "/src/assets/no-actor.png",
+  noImage: "/src/assets/no-image.png",
+};
