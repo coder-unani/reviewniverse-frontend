cc1ab85e725fdbd38b65358858d258cde9529489
hwakk <97441379+hwakk@users.noreply.github.com>
2024-06-26 05:10:48 +0900

    [feat] 리뷰 수정하기 추가

--- src/components/Modal/ReviewModal.jsx
+++ src/components/Modal/ReviewModal.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from "react";
+import React, { useEffect, useRef } from "react";
 import Modal from "/src/components/Modal/Modal";
 import * as Yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
@@ -7,16 +7,17 @@ import { RiCloseLine } from "@remixicon/react";
 import HttpClient from "/src/utils/HttpClient";
 import "/src/styles/ReviewModal.css";
 import { cLog, cError } from "/src/utils/test";
+import { isEmpty, set } from "lodash";
 
 const API_BASE_URL = "https://comet.orbitcode.kr/v1";
 
 /**
  * TODO:
- * 1. 리뷰 등록 성공시 myInfo 상태값에 리뷰 추가하기
+ * 1. 리뷰 등록/수정 성공시 myInfo 상태값에 리뷰 추가하기
  */
 
 const ReviewModal = (props) => {
-  const { content, onClose } = props;
+  const { content, myReview, onClose } = props;
   const modalRef = useRef();
 
   // 리뷰 모달 바깥 영역 클릭시 모달 닫기
@@ -51,37 +52,74 @@ const ReviewModal = (props) => {
     handleSubmit,
     control,
     formState: { isDirty, isValid },
+    setValue,
+    setFocus,
+    trigger,
   } = methods;
 
   const watchedTitle = useWatch({ control, name: "title", defaultValue: "" });
 
   const onSubmit = handleSubmit((data) => {
-    cLog(data);
-
-    // 리뷰 등록하기
-    try {
-      const access_token = sessionStorage.getItem("access_token");
-      // 디벨롭 필요
-      if (!access_token) throw new Error("로그인이 필요한 서비스입니다.");
-      const client = new HttpClient(access_token);
-      client
-        .post(`${API_BASE_URL}/contents/videos/${content.id}/reviews`, {
-          title: data.title,
-          content: "",
-          is_spoiler: data.spoiler,
-          is_private: data.private,
-        })
-        .then((res) => {
-          if (res.status === 201) {
-            // res.code === "REVIEW_CREATE_SUCC"
-            cLog("리뷰가 등록되었습니다.");
-            onClose();
-          }
-        });
-    } catch (error) {
-      cError(error);
+    if (isEmpty(myReview)) {
+      // 리뷰 등록하기
+      try {
+        const access_token = sessionStorage.getItem("access_token");
+        // 디벨롭 필요
+        if (!access_token) throw new Error("로그인이 필요한 서비스입니다.");
+        const client = new HttpClient(access_token);
+        client
+          .post(`${API_BASE_URL}/contents/videos/${content.id}/reviews`, {
+            title: data.title,
+            content: "",
+            is_spoiler: data.spoiler,
+            // is_private: data.private,
+          })
+          .then((res) => {
+            if (res.status === 201) {
+              // res.code === "REVIEW_CREATE_SUCC"
+              cLog("리뷰가 등록되었습니다.");
+              onClose();
+            }
+          });
+      } catch (error) {
+        cError(error);
+      }
+    } else {
+      // 리뷰 수정하기
+      try {
+        const access_token = sessionStorage.getItem("access_token");
+        // 디벨롭 필요
+        if (!access_token) throw new Error("로그인이 필요한 서비스입니다.");
+        const client = new HttpClient(access_token);
+        client
+          .put(`${API_BASE_URL}/contents/videos/${content.id}/reviews/${myReview.id}`, {
+            title: data.title,
+            content: "",
+            is_spoiler: data.spoiler,
+            // is_private: data.private,
+          })
+          .then((res) => {
+            if (res.status === 204) {
+              // res.code === "REVIEW_UPDATE_SUCC"
+              cLog("리뷰가 수정되었습니다.");
+              onClose();
+            }
+          });
+      } catch (error) {
+        cError(error);
+      }
     }
   });
+
+  useEffect(() => {
+    if (isEmpty(myReview)) return;
+
+    setValue("title", myReview.title, { shouldDirty: true });
+    setValue("spoiler", myReview.is_spoiler);
+    setValue("private", myReview.is_private);
+
+    trigger();
+  }, [myReview, setValue, trigger, setFocus]);
 
   return (
     <Modal>
@@ -111,7 +149,7 @@ const ReviewModal = (props) => {
                   <span>{watchedTitle.length}</span> / 10000
                 </p>
                 <button type="submit" className="save" disabled={!isDirty || !isValid}>
-                  저장
+                  {myReview ? "수정" : "등록"}
                 </button>
               </div>
             </div>
