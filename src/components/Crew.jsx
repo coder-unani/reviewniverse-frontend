aaad717eaaaf322306797f10ae10424d5799af9c
hwakk <97441379+hwakk@users.noreply.github.com>
2024-06-26 04:17:26 +0900

    [feat] 통합된 Crew 컴포넌트 추가 (출연진 및 스태프)

---
+++ src/components/Crew.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import { DEFAULT_IMAGES } from "/src/config/images";
+
+const Crew = (props) => {
+  const { crew, type } = props;
+
+  return (
+    <div className="crew" key={crew.id}>
+      <figure className="image">
+        <LazyLoadImage src={crew.picture || DEFAULT_IMAGES.noActor} alt={crew.name} effect="blur" />
+      </figure>
+      <div className="name-wrapper">
+        <span className="name">{crew.name}</span>
+        <div className="role">
+          <span>{type(crew.type)}</span>
+          {crew.role && (
+            <>
+              <span>|</span>
+              <span>{crew.role}</span>
+            </>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Crew;
