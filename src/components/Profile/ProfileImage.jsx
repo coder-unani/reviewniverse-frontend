db86df285b4ebea67fd42ee01a6bb83d747d399e
hwakk <97441379+hwakk@users.noreply.github.com>
2024-06-26 22:06:57 +0900

    [feat] ProfileImage 컴포넌트 분리

---
+++ src/components/Profile/ProfileImage.jsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+import { DEFAULT_IMAGES } from "/src/config/images";
+import "/src/styles/ProfileImage.css";
+
+const ProfileImage = (props) => {
+  const { image, size } = props;
+  const profileImage = image || DEFAULT_IMAGES.noProfile;
+  const style = size ? { width: `${size}px`, height: `${size}px` } : {};
+
+  return (
+    <figure className="profile-image" style={style}>
+      <img src={profileImage} alt="프로필 이미지" />
+    </figure>
+  );
+};
+
+export default ProfileImage;
