0fe082936c919712fee28e798380eef0facd3db9
hwakk <97441379+hwakk@users.noreply.github.com>
2024-06-27 02:21:09 +0900

    [refactor] 기본 이미지 설정 업데이트

--- src/utils/contentFormat.js
+++ src/utils/contentFormat.js
@@ -1,16 +1,10 @@
 import { find, isEmpty } from "lodash";
 import { VIDEO_ACTOR_TYPE, VIDEO_STAFF_TYPE, COUNTRY_CODE } from "/src/config/types";
-
-const IMAGES = {
-  noActor: "/src/assets/no-actor.png",
-  noImage: "/src/assets/no-image.png",
-  logo: "/src/assets/logo.svg",
-  logoWhite: "/src/assets/logo-w.svg",
-};
+import { DEFAULT_IMAGES } from "/src/config/images";
 
 // 배경 이미지 포맷
 export const formatBackgroundImage = (images) => {
-  if (isEmpty(images)) return IMAGES.noImage;
+  if (isEmpty(images)) return DEFAULT_IMAGES.noImage;
   // 썸네일 이미지 배열 중에서 type이 11인 이미지만 렌더링
   const backgroundImage = find(images, { type: "11" });
   return backgroundImage.url;
@@ -18,7 +12,7 @@ export const formatBackgroundImage = (images) => {
 
 // 포스터 이미지 포맷
 export const formatPoster = (images) => {
-  if (isEmpty(images)) return IMAGES.noImage;
+  if (isEmpty(images)) return DEFAULT_IMAGES.noImage;
   // 이미지 배열 중에서 type이 10인 이미지만 렌더링
   // type이 10인 이미지가 없을 경우 type이 11인 이미지 렌더링
   const thumbnail = find(images, { type: "10" }) ?? find(images, { type: "11" });
