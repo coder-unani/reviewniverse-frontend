d6d2a0d358eea7744eb9b2e844529da0bcd8ce42
hwakk <97441379+hwakk@users.noreply.github.com>
2024-06-27 02:36:20 +0900

    [feat] 기본 레이아웃에 scrollToTop 기능 추가

--- src/layouts/default/index.jsx
+++ src/layouts/default/index.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import Header from "/src/layouts/default/header";
 import Footer from "/src/layouts/default/footer";
+import ScrollToTop from "/src/components/ScrollToTop";
 
 const DefaultLayout = () => {
   return (
     <div className="wrapper">
+      <ScrollToTop />
       <Header />
       <Outlet />
       <Footer />
