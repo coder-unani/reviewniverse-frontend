e2395e55843c70570ca8619d8c759258aabe9dec
hwakk <97441379+hwakk@users.noreply.github.com>
2024-06-26 21:08:25 +0900

    [feat] 리뷰 좋아요 기능 추가, 좋아요 버튼 활성화 표시

--- src/components/Review.jsx
+++ src/components/Review.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ProfileButton from "/src/components/Button/ProfileButton";
 import { RiStarFill, RiThumbUpLine, RiThumbUpFill } from "@remixicon/react";
 import { diffDate } from "/src/utils/format";
@@ -10,8 +10,12 @@ import { formatRating } from "/src/utils/contentFormat";
  * 2. 리뷰 클릭시 리뷰 모달 열기
  */
 
+const API_BASE_URL = "https://comet.orbitcode.kr/v1";
+
 const Review = (props) => {
-  const { review } = props;
+  const { review, isLike, onLikeClick } = props;
+
+  const handleReviewLike = (reviewId) => onLikeClick(reviewId);
 
   return (
     <div className="review">
@@ -29,8 +33,8 @@ const Review = (props) => {
       </div>
       <div className="bottom">
         <span>{diffDate(review.created_at)}</span>
-        <button className="like">
-          <RiThumbUpLine size={16} />
+        <button className="like" onClick={() => handleReviewLike(review.id)}>
+          {isLike ? <RiThumbUpFill size={16} /> : <RiThumbUpLine size={16} />}
           <span>{review.like_count}</span>
         </button>
       </div>
