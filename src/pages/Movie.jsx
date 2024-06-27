6007339cee1bfe3e571149c0d739aaef9da5c669
hwakk <97441379+hwakk@users.noreply.github.com>
2024-06-27 23:12:08 +0900

    [feat] AllList 컴포넌트 추가

--- src/pages/Movie.jsx
+++ src/pages/Movie.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AllRanking from "/src/components/AllRanking";
+import AllList from "/src/components/AllList";
 
 const Movie = () => {
   return (
     <main className="main">
       <AllRanking type={10} />
+      <AllList type={10} />
     </main>
   );
 };
--- src/pages/Series.jsx
+++ src/pages/Series.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import AllRanking from "/src/components/AllRanking";
+import AllList from "/src/components/AllList";
 
 const Series = () => {
   return (
     <main className="main">
       <AllRanking type={11} />
+      <AllList type={11} />
     </main>
   );
 };
