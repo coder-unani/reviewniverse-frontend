75c36504b324b2d5f7859fee47d0634e7ac6c2f8
hwakk <97441379+hwakk@users.noreply.github.com>
2024-06-30 07:41:05 +0900

    [feat] 검색 페이지 컴포넌트 추가
    - 키워드 입력을 받아 영화 데이터 검색하는 기능 추가
    - 검색 결과가 없을 경우 처리하는 렌더링 추가
    - HttpClient를 사용하여 API 호출 구현
    - LazyLoadImage를 사용하여 이미지 지연 로딩 구현
    - 검색 결과 UI 스타일 적용

---
+++ src/pages/Search.jsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import HttpClient from "/src/utils/HttpClient";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import { formatYear } from "/src/utils/format";
+import { formatPoster, formatCountry } from "/src/utils/contentFormat";
+import { isEmpty, set } from "lodash";
+import { DEFAULT_IMAGES } from "/src/config/images";
+import { cLog, cError } from "/src/utils/test";
+import "/src/styles/Search.css";
+
+const API_BASE_URL = "https://comet.orbitcode.kr/v1";
+
+const Search = () => {
+  const location = useLocation();
+  const params = new URLSearchParams(location.search);
+  const query = params.get("query");
+  const [searchMovies, setSearchMovies] = useState([]);
+
+  const renderEmpty = () => {
+    return (
+      <div className="empty">
+        <img src={DEFAULT_IMAGES.noSearch} alt="검색 결과 없음" />
+        <p className="title">
+          "<em>{query}</em>"에 대한 검색 결과가 없어요.
+        </p>
+        <p className="sub-title">입력한 검색어를 다시 한번 확인해주세요.</p>
+      </div>
+    );
+  };
+
+  const renderMovies = () => {
+    return (
+      <div className="search-list-wrapper">
+        <div className="list-wrapper">
+          {searchMovies.map((movie, index) => (
+            <article className="content" key={index}>
+              <Link to={`/contents/${movie.id}`}>
+                <div className="img-wrapper">
+                  <figure className="thumbnail">
+                    <LazyLoadImage src={formatPoster(movie.thumbnail)} alt="썸네일" effect="blur" />
+                  </figure>
+                </div>
+                <div className="info">
+                  <p className="title">{movie.title}</p>
+                  <div className="sub-title">
+                    <span>{formatYear(movie.release)}</span>
+                    <span>|</span>
+                    <span>{formatCountry(movie.country)}</span>
+                  </div>
+                </div>
+              </Link>
+            </article>
+          ))}
+        </div>
+      </div>
+    );
+  };
+
+  const fetchData = async () => {
+    try {
+      const client = new HttpClient();
+      const res = await client.get(`${API_BASE_URL}/contents/videos`, {
+        q: query,
+      });
+      if (res.status === 200) {
+        setSearchMovies(res.data.data);
+      } else if (res.status === 404) {
+        cLog("검색 결과가 없습니다.");
+        setSearchMovies([]);
+      }
+    } catch (error) {
+      cError(error);
+      setSearchMovies([]);
+    }
+  };
+
+  useEffect(() => {
+    if (!query) return;
+
+    fetchData(query);
+  }, [query, location]);
+
+  return (
+    <main className="search-main">
+      <section className="search-result">
+        <p>"{query}"의 검색결과</p>
+      </section>
+      <section className="search-contents">{isEmpty(searchMovies) ? renderEmpty() : renderMovies()}</section>
+    </main>
+  );
+};
+
+export default Search;
