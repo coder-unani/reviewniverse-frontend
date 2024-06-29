c219dc461a94475ea864ace1996a76c64da2ee1f
hwakk <97441379+hwakk@users.noreply.github.com>
2024-06-29 22:42:26 +0900

    [feat] 사용자 정보 페이지 구현
    - 배경 이미지, 프로필 이미지 및 사용자 프로필 세부 정보가 포함된 사용자 정보 페이지 구현
    - 사용자 등급, 리뷰 및 좋아요 수 표시
    - 로그인한 사용자에 대해서만 프로필 편집 링크 표시

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -2,47 +2,85 @@ import React, { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import HttpClient from "/src/utils/HttpClient";
 import ProfileImage from "/src/components/Button/Profile/ProfileImage";
+import SettingButton from "/src/components/Button/Setting";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
-import { RiArrowRightSLine } from "@remixicon/react";
 import { formatNumber } from "/src/utils/format";
 import { isEmpty } from "lodash";
 import { DEFAULT_IMAGES } from "/src/config/images";
 import "/src/styles/User.css";
 import { cLog, cError } from "/src/utils/test";
 
+const API_BASE_URL = "https://comet.orbitcode.kr/v1";
+
 const User = () => {
   const { userId } = useParams();
   const [user, setUser] = useState({});
-  const [loginUser, setLoginUser] = useState(() => {
-    const storedUser = sessionStorage.getItem("user");
-    return storedUser ? JSON.parse(storedUser) : null;
-  });
+  const [isLogin, setIsLogin] = useState(false);
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const client = new HttpClient();
-        client.get("https://comet.orbitcode.kr/v1/users", { uid: parseInt(userId) }).then((res) => {
-          if (res.status === 200) {
-            // res.code === "SEARCH_SUCC"
-            setUser(res.data.data[0]);
-          } else {
-            cLog("사용자 정보를 불러오는데 실패하였습니다.");
-            return;
-          }
-        });
-      } catch (error) {
-        cError(error);
+  // 토큰 검증
+  const tokenValidation = async () => {
+    const access_token = sessionStorage.getItem("access_token");
+    if (!access_token) return false;
+
+    const client = new HttpClient(access_token);
+
+    try {
+      const res = await client.get(`${API_BASE_URL}/token`);
+      if (res.status === 200) {
+        return true;
+      } else {
+        sessionStorage.removeItem("user");
+        sessionStorage.removeItem("access_token");
+        setLoginUser(null);
+        return false;
       }
-    };
+    } catch (error) {
+      sessionStorage.removeItem("user");
+      sessionStorage.removeItem("access_token");
+      setLoginUser(null);
+      return false;
+    }
+  };
 
-    fetchData();
-  }, []);
+  // 사용자 정보 가져오기
+  const fetchData = async () => {
+    try {
+      const client = new HttpClient();
+      client.get(`${API_BASE_URL}/users`, { uid: parseInt(userId) }).then((res) => {
+        if (res.status === 200) {
+          // res.code === "SEARCH_SUCC"
+          setUser(res.data.data[0]);
+        } else {
+          cLog("사용자 정보를 불러오는데 실패하였습니다.");
+          return;
+        }
+      });
+    } catch (error) {
+      cError(error);
+    }
+  };
 
   useEffect(() => {
-    console.log(loginUser.id);
-  }, [loginUser]);
+    // 저장된 로그인 유저 정보 가져오기
+    const storedUser = sessionStorage.getItem("user");
+    if (storedUser) {
+      // 로그인한 유저가 있다면 userId와 로그인한 유저가 같은지 확인
+      const user = JSON.parse(storedUser);
+      if (user.id === parseInt(userId)) {
+        // 같다면 토큰 검증
+        tokenValidation().then((isValid) => {
+          if (!isValid) return;
+          setUser(user);
+          setIsLogin(true);
+          return;
+        });
+      }
+    } else {
+      // 로그인한 유저 정보가 없으면 API 요청
+      fetchData();
+    }
+  }, []);
 
   if (isEmpty(user)) return null;
 
@@ -51,20 +89,14 @@ const User = () => {
       <div className="user-wrapper">
         <div className="user">
           <section className="user-info">
+            {isLogin && <SettingButton />}
             <figure className="background">
               <LazyLoadImage src={DEFAULT_IMAGES.noImage} alt="배경 이미지" effect="blur" />
             </figure>
             <div className="user-profile">
               <ProfileImage image={user.profile_image} size={100} />
-              {loginUser && loginUser.id === user.id ? (
-                <Link to="/user/profile" className="edit-profile">
-                  <h1 className="nickname">{user.nickname}</h1>
-                  <RiArrowRightSLine size={20} />
-                </Link>
-              ) : (
-                <h1 className="nickname">{user.nickname}</h1>
-              )}
-              {user.profile_text && <p>{user.profile_text}</p>}
+              <h1 className="nickname">{user.nickname}</h1>
+              {user.profile_text && <p className="introduction">{user.profile_text}</p>}
             </div>
             <div className="user-contents">
               <Link to="">
