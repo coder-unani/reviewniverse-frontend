540954f86bd5b271432850966d07c068783c2275
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-05 05:32:38 +0900

    [refactor] MobileContext 삭제

--- src/layouts/default/index.jsx
+++ src/layouts/default/index.jsx
@@ -3,18 +3,15 @@ import { Outlet } from "react-router-dom";
 import Header from "/src/layouts/default/header";
 import Footer from "/src/layouts/default/footer";
 import ScrollToTop from "/src/components/ScrollToTop";
-import { MobileContextProvider } from "../../context/MobileContext";
 
 const DefaultLayout = () => {
   return (
-    <MobileContextProvider>
+    <div className="wrapper">
       <ScrollToTop />
-      <div className="wrapper">
-        <Header />
-        <Outlet />
-        <Footer />
-      </div>
-    </MobileContextProvider>
+      <Header />
+      <Outlet />
+      <Footer />
+    </div>
   );
 };
 
