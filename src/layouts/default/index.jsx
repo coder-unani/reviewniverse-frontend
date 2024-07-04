779e2e91ca13c3f8c2a4eb8767648b363f388d5f
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-05 04:52:14 +0900

    [refactor] DefaultLayout에 MobileContextProvider 추가

--- src/layouts/default/index.jsx
+++ src/layouts/default/index.jsx
@@ -3,15 +3,18 @@ import { Outlet } from "react-router-dom";
 import Header from "/src/layouts/default/header";
 import Footer from "/src/layouts/default/footer";
 import ScrollToTop from "/src/components/ScrollToTop";
+import { MobileContextProvider } from "../../context/MobileContext";
 
 const DefaultLayout = () => {
   return (
-    <div className="wrapper">
+    <MobileContextProvider>
       <ScrollToTop />
-      <Header />
-      <Outlet />
-      <Footer />
-    </div>
+      <div className="wrapper">
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
+    </MobileContextProvider>
   );
 };
 
