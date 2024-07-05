f76619dc71549cd95e6ff187940fd40ce3b39630
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-06 05:39:17 +0900

    [feat] 최근 검색어 로컬 스토리지 저장 기능 추가

--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { RiSearchLine } from "@remixicon/react";
+import { recentSaveLoaclStorage, sliceLocalStorage } from "/src/utils/localStorage";
 
 const SearchForm = () => {
   const location = useLocation();
@@ -17,21 +18,34 @@ const SearchForm = () => {
     navigate(`/search?query=${searchQuery}`);
   };
 
+  // 최근 검색어 로컬 스토리지에 저장 (최대 5개)
+  const saveKeyword = (keyword) => {
+    recentSaveLoaclStorage("RECENT_SEARCH_KEYWORDS", keyword);
+    sliceLocalStorage("RECENT_SEARCH_KEYWORDS", 5);
+  };
+
   useEffect(() => {
     const path = location.pathname;
 
     if (!searchInputRef.current) return;
 
     // 검색 페이지가 아닌 경우 검색어 초기화
-    if (path !== "/search" && searchInputRef.current) {
+    if (path !== "/search") {
       searchInputRef.current.value = "";
     }
 
     // 검색 쿼리가 있음에도 검색어 입력란에 값이 없는 경우
-    if (path === "/search" && searchInputRef.current) {
+    if (path === "/search") {
       const params = new URLSearchParams(location.search);
       const query = params.get("query");
       searchInputRef.current.value = query;
+
+      // 검색어 입력란에 포커스
+      if (query) {
+        saveKeyword(query);
+      } else {
+        searchInputRef.current.focus();
+      }
     }
   }, [location]);
 
