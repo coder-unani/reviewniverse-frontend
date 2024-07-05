b5a79124f905e6c6f8a479f28bbe7d65c4d67427
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-06 02:47:15 +0900

    [refactor] SearchRoutes 추가

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,8 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 
 import MainRoutes from "/src/routes/MainRoutes";
+import SearchRoutes from "/src/routes/SearchRoutes";
 import UserRoutes from "/src/routes/UserRoutes";
 
-const router = createBrowserRouter([MainRoutes, UserRoutes]);
+const router = createBrowserRouter([MainRoutes, SearchRoutes, UserRoutes]);
 
 export default router;
