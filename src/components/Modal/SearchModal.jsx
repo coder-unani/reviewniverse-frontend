ef643a5d3779ab207614958f5efb29c1f0ac1058
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-06 05:40:16 +0900

    [feat] 모바일 화면시 검색 모달 화면 추가 구현
    - 최근 검색어 표시
    - 최근 검색어 전체 삭제 기능

---
+++ src/components/Modal/SearchModal.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect, useState } from "react";
+import Modal from "/src/components/Modal";
+import { Link } from "react-router-dom";
+import { isEmpty } from "lodash";
+import { getLocalStorage, clearLocalStorage } from "/src/utils/localStorage";
+import "/src/styles/SearchModal.css";
+
+const SearchModal = () => {
+  const [recent, setRecent] = useState([]);
+
+  // 최근 검색어 전체 삭제
+  const handleClear = () => {
+    clearLocalStorage("RECENT_SEARCH_KEYWORDS");
+    setRecent([]);
+  };
+
+  const renderRecent = () => {
+    return (
+      <div className="recent">
+        <div className="top">
+          <p>최근 검색어</p>
+          <button className="clear" onClick={handleClear}>
+            모두 삭제
+          </button>
+        </div>
+        <div className="bottom">
+          <ul>
+            {recent.map((keyword, index) => (
+              <li key={index}>
+                <Link to={`/search?query=${keyword}`}>{keyword}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
+  };
+
+  useEffect(() => {
+    const recentKeywords = getLocalStorage("RECENT_SEARCH_KEYWORDS");
+    setRecent(recentKeywords);
+  }, []);
+
+  return (
+    <Modal>
+      <div className="search-modal">
+        <div className="search">
+          {!isEmpty(recent) && renderRecent()}
+          {/* 인기 검색어 */}
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default SearchModal;
