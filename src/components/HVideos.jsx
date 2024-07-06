f77514ba84ce2543331e13374f2b73abe4ffe5c9
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-06 09:16:51 +0900

    [feat] swiper video를 표시하기 위한 HVideos 구성 요소 추가

---
+++ src/components/HVideos.jsx
@@ -0,0 +1,85 @@
+import React, { useState, useEffect } from "react";
+import VideoRankItem from "/src/components/VideoRankItem";
+import VideoItem from "/src/components/VideoItem";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import { RiArrowRightSLine, RiArrowLeftSLine } from "@remixicon/react";
+import { isEmpty } from "lodash";
+import "/src/styles/HVideos.css";
+
+const HVideos = (props) => {
+  const { content } = props;
+  const [videos, setVideos] = useState([]);
+
+  // 스와이퍼 설정
+  const swiperConfig = (prevEl, nextEl) => ({
+    modules: [Navigation],
+    spaceBetween: 8,
+    slidesPerView: 3.01,
+    slidesPerGroup: 3,
+    speed: 1000,
+    navigation: { prevEl, nextEl },
+    allowTouchMove: true,
+    breakpoints: {
+      577: {
+        slidesPerView: 3,
+        allowTouchMove: false,
+      },
+      769: {
+        spaceBetween: 10,
+        slidesPerView: 4,
+        slidesPerGroup: 4,
+        allowTouchMove: false,
+      },
+      1025: {
+        spaceBetween: 12,
+        slidesPerView: 5,
+        slidesPerGroup: 5,
+        allowTouchMove: false,
+      },
+    },
+  });
+
+  useEffect(() => {
+    if (!content) return;
+    setVideos(content.content.list);
+  }, [content]);
+
+  if (isEmpty(videos)) return null;
+
+  return (
+    <section className="horizontal-videos-wrapper">
+      <div className="title-wrapper">
+        <h2 className="title">{content.title}</h2>
+        {/* <button className="more">
+          더보기
+          <RiArrowRightSLine size={20} />
+        </button> */}
+      </div>
+      <div className="swiper-container">
+        <Swiper {...swiperConfig(`.prev-${content.scn_code}`, `.next-${content.scn_code}`)}>
+          {content.content.template === "default"
+            ? videos.map((video, index) => (
+                <SwiperSlide key={index}>
+                  <VideoItem video={video} index={index} />
+                </SwiperSlide>
+              ))
+            : videos.map((video, index) => (
+                <SwiperSlide key={index}>
+                  <VideoRankItem video={video} index={index} />
+                </SwiperSlide>
+              ))}
+        </Swiper>
+        <div className={`swiper-button-prev prev-${content.scn_code}`}>
+          <RiArrowLeftSLine size={24} />
+        </div>
+        <div className={`swiper-button-next next-${content.scn_code}`}>
+          <RiArrowRightSLine size={24} />
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HVideos;
