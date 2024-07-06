f2d5ae71a27c31f6394e93591950cfe69b10fabb
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-06 09:15:53 +0900

    [feat] video 정보를 표시하기 위한 VideoItem 구성 요소 추가

---
+++ src/components/VideoItem.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import { formatYear } from "/src/utils/format";
+import { formatPoster, formatCountry } from "/src/utils/contentFormat";
+
+const VideoItem = (props) => {
+  const { video } = props;
+
+  return (
+    <article className="default-content">
+      <Link to={`/contents/${video.id}`}>
+        <div className="img-wrapper">
+          <figure className="thumbnail">
+            <LazyLoadImage src={formatPoster(video.thumbnail)} alt="썸네일" effect="blur" />
+          </figure>
+        </div>
+        <div className="info">
+          <p className="title">{video.title}</p>
+          <div className="sub-title">
+            <span>{formatYear(video.release)}</span>
+            <span>|</span>
+            <span>{formatCountry(video.country)}</span>
+          </div>
+        </div>
+      </Link>
+    </article>
+  );
+};
+
+export default VideoItem;
