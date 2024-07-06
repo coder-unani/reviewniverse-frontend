da3c564da1b62aa2fc0308bcd0a00af1dcb92ba9
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-06 09:15:24 +0900

    [feat] video 랭킹 표시하기 위한 VideoRankItem 구성 요소 추가

---
+++ src/components/VideoRankItem.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+import { formatYear } from "/src/utils/format";
+import { formatPoster, formatCountry } from "/src/utils/contentFormat";
+
+const VideoRankItem = (props) => {
+  const { video, index } = props;
+
+  // 랭킹 숫자 포맷
+  const formatRankingNumber = (number) => {
+    // 숫자 한자리씩 잘라서 배열에 저장
+    const numbers = number.toString().split("");
+    // 배열 반복해서 number-{}.svg 이미지 추가해서 반환
+    return numbers.map((num, index) => <img key={index} src={`/assets/number-${num}.svg`} alt={num} />);
+  };
+
+  return (
+    <article className="rank-content">
+      <Link to={`/contents/${video.id}`}>
+        <div className="img-wrapper">
+          <figure className="thumbnail">
+            <LazyLoadImage src={formatPoster(video.thumbnail)} alt="썸네일" effect="blur" />
+          </figure>
+          <div className="number">{formatRankingNumber(index + 1)}</div>
+        </div>
+        <div className="info">
+          <p className="title">{video.title}</p>
+          <div className="sub-title">
+            <span>{formatYear(video.release)}</span>
+            <span>|</span>
+            <span>{formatCountry(video.country)}</span>
+          </div>
+        </div>
+      </Link>
+    </article>
+  );
+};
+
+export default VideoRankItem;
