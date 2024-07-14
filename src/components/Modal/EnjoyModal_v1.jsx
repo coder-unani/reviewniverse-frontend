10baed5ac1efafa3c17f7fbb06eabae1f8795be0
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-14 23:46:06 +0900

    [feat] EnjoyModal 이전 버전(회원가입 적용) 저장

---
+++ src/components/Modal/EnjoyModal_v1.jsx
@@ -0,0 +1,44 @@
+import React, { useRef } from "react";
+import Modal from "/src/components/Modal";
+import { Link } from "react-router-dom";
+import { RiCloseLine } from "@remixicon/react";
+import Enjoy from "/assets/enjoy.png";
+import "/src/styles/EnjoyModal.css";
+
+const EnjoyModal = (props) => {
+  const { onClose } = props;
+  const modalRef = useRef();
+
+  // 모달 바깥 영역 클릭시 모달 닫기
+  const handleModalClose = (e) => {
+    if (e.target === modalRef.current) onClose();
+  };
+
+  const handleCloseButton = () => {
+    onClose();
+  };
+
+  return (
+    <Modal>
+      <div className="enjoy-modal" ref={modalRef} onClick={handleModalClose}>
+        <div className="enjoy">
+          <RiCloseLine size={28} className="close" onClick={handleCloseButton} />
+          <div className="content">
+            <img src={Enjoy} alt="enjoy" />
+            <p>로그인이 필요한 기능이에요!</p>
+          </div>
+          <div className="button-wrapper">
+            <Link to="/user/join" className="join">
+              회원가입
+            </Link>
+            <Link to="/user/login" className="login">
+              로그인
+            </Link>
+          </div>
+        </div>
+      </div>
+    </Modal>
+  );
+};
+
+export default EnjoyModal;
