fbe77f9630aa5b1ec727c5ca3f9a8c94ebf877dc
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-15 16:00:38 +0900

    [refactor] 이전 비디오 목록에 새로운 비디오 목록 추가하는 로직 구현

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,18 @@
-import React, { useState } from "react";
-import { SCREEN_MAIN_ID } from "/src/config/types";
+import React, { useEffect, useState } from "react";
 import VideoPage from "/src/components/VideoPage";
-import "/src/styles/Home.css";
+import { SCREEN_MAIN_ID } from "/src/config/types";
+import { VIDEO_ORDER_OPTIONS } from "/src/config/constants";
 import { useVideos } from "/src/hooks/useVideos";
 import { useScreenContents } from "/src/hooks/useScreenContents";
+import { arrayRandomValue } from "/src/utils/format";
+import { isEmpty } from "lodash";
+import "/src/styles/Home.css";
 
 const Home = () => {
   const [page, setPage] = useState(1);
+  const [orderBy, setOrderBy] = useState(arrayRandomValue(VIDEO_ORDER_OPTIONS));
+  const [videos, setVideos] = useState([]);
+
   // 스크린 데이터
   const {
     data: screens,
@@ -15,10 +21,22 @@ const Home = () => {
   } = useScreenContents({ code: SCREEN_MAIN_ID });
 
   // 비디오 리스트
-  const orderBy = "view_desc";
-  const { data: videos, error: videosError, isLoading: videosIsLoading } = useVideos({ page, orderBy });
+  const { data: videosData, error: videosError, isLoading: videosIsLoading } = useVideos({ page, orderBy });
 
-  // 로딩중일때 표시할 화면
+  // 페이지 변경
+  const handlePage = (page) => {
+    setPage(page);
+  };
+
+  useEffect(() => {
+    if (isEmpty(videosData)) return;
+    setVideos((prev) => {
+      if (isEmpty(prev)) return videosData;
+      return { ...prev, data: [...prev.data, ...videosData.data] };
+    });
+  }, [page, videosData]);
+
+  // 로딩중일때 표시할 화면 (스켈레톤 UI)
   if (videosIsLoading || screensIsLoading) {
   }
 
@@ -27,10 +45,6 @@ const Home = () => {
   }
   if (screensError) {
   }
-
-  const handlePage = (page) => {
-    setPage(page);
-  };
 
   // 데이터 props로 하위 컴포넌트에 전달
   return <VideoPage screens={screens} videos={videos} handlePage={handlePage} />;
--- src/pages/Series.jsx
+++ src/pages/Series.jsx
@@ -1,9 +1,53 @@
-import React from "react";
-import { SCREEN_SERIES_ID } from "/src/config/types";
+import React, { useEffect, useState } from "react";
 import VideoPage from "/src/components/VideoPage";
+import { SCREEN_SERIES_ID } from "/src/config/types";
+import { useVideos } from "/src/hooks/useVideos";
+import { VIDEO_ORDER_OPTIONS } from "/src/config/constants";
+import { useScreenContents } from "/src/hooks/useScreenContents";
+import { arrayRandomValue } from "/src/utils/format";
+import { isEmpty } from "lodash";
 
 const Movie = () => {
-  return <VideoPage id={SCREEN_SERIES_ID} type={11} />;
+  const code = 11;
+  const [page, setPage] = useState(1);
+  const [orderBy, setOrderBy] = useState(arrayRandomValue(VIDEO_ORDER_OPTIONS));
+  const [videos, setVideos] = useState([]);
+
+  // 스크린 데이터
+  const {
+    data: screens,
+    error: screensError,
+    isLoading: screensIsLoading,
+  } = useScreenContents({ code: SCREEN_SERIES_ID });
+
+  // 비디오 리스트
+  const { data: videosData, error: videosError, isLoading: videosIsLoading } = useVideos({ page, orderBy, code });
+
+  // 페이지 변경
+  const handlePage = (page) => {
+    setPage(page);
+  };
+
+  useEffect(() => {
+    if (isEmpty(videosData)) return;
+    setVideos((prev) => {
+      if (isEmpty(prev)) return videosData;
+      return { ...prev, data: [...prev.data, ...videosData.data] };
+    });
+  }, [page, videosData]);
+
+  // 로딩중일때 표시할 화면 (스켈레톤 UI)
+  if (videosIsLoading || screensIsLoading) {
+  }
+
+  // 에러일때 표시할 화면
+  if (videosError) {
+  }
+  if (screensError) {
+  }
+
+  // 데이터 props로 하위 컴포넌트에 전달
+  return <VideoPage screens={screens} videos={videos} handlePage={handlePage} />;
 };
 
 export default Movie;
