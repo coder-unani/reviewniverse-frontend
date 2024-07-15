e4fd1ae36a462d662589f57347c44f55554c520c
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-15 15:48:32 +0900

    [refactor] 사용하지 않는 import 삭제

--- src/components/VideoPage.jsx
+++ src/components/VideoPage.jsx
@@ -1,15 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import HVideos from "/src/components/HVideos";
 import Videos from "/src/components/Videos";
-import { cLog, cError } from "/src/utils/test";
-
-const API_BASE_URL = "https://comet.orbitcode.kr/v1";
 
 const VideoPage = ({ screens, videos, handlePage }) => {
   return (
     <main className="main">
       {screens && screens.map((content, index) => <HVideos key={index} content={content} />)}
-      {/* <Videos {...(type && { type })} /> */}
+      <Videos videos={videos} handlePage={handlePage} />
     </main>
   );
 };
