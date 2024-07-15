1cc33fc37e38d0190e5bf5a3e3c8238f17d0aaed
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-15 15:45:00 +0900

    [refactor] settings에 API_BASE_URL 추가

--- src/config/endpoints.js
+++
@@ -1,2 +0,0 @@
-// API URL
-const API_BASE_URL = "https://comet.orbitcode.kr/v1";
--- src/config/settings.js
+++ src/config/settings.js
@@ -1,5 +1,12 @@
 // APP 설정 파일
-
 export const settings = {
   DEBUG: import.meta.env.VITE_DEBUG === "true",
+  API_BASE_URL: "https://comet.orbitcode.kr",
+  FIREBASE_API_KEY: import.meta.env.VITE_FIREBASE_API_KEY,
+  FIREBASE_AUTH_DOMAIN: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  FIREBASE_PROJECT_ID: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  FIREBASE_STORAGE_BUCKET: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  FIREBASE_MESSAGING_SENDER_ID: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  FIREBASE_APP_ID: import.meta.env.VITE_FIREBASE_APP_ID,
+  FIREBASE_MEASUREMENT_ID: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
