36a4673a9cf4572b6cd50d698ba69e5106129dd6
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-16 22:43:31 +0900

    [refactor] endpoints 상수 추가, API url를 endpoints 상수 사용으로 변경

--- src/api/screens.jsx
+++ src/api/screens.jsx
@@ -2,10 +2,15 @@ import HttpClient from "/src/utils/HttpClient";
 import { settings } from "/src/config/settings";
 import { cLog, cError } from "/src/utils/test";
 
+const baseURL = settings.API_BASE_URL;
+const endpoints = {
+  screens: baseURL + "/v1/screens",
+};
+
 export const fetchScreenContents = async ({ code }) => {
   try {
     const client = new HttpClient();
-    const res = await client.get(`${settings.API_BASE_URL}/v1/screens`, {
+    const res = await client.get(endpoints.screens, {
       code,
     });
     return res.status === 200 ? res.data : [];
