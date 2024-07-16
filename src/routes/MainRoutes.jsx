2a0ec7e8cbc883217f87ae5a5ba60bd33d22365d
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-16 22:25:25 +0900

    [refactor] MainRoutes에 UserRoutes 병합

--- src/routes/MainRoutes.jsx
+++ src/routes/MainRoutes.jsx
@@ -3,24 +3,18 @@ import Home from "/src/pages/Home";
 import Movie from "/src/pages/Movie";
 import Series from "/src/pages/Series";
 import Content from "/src/pages/Content";
-import EditUser from "/src/pages/EditUser";
-import DeleteUser from "/src/pages/DeleteUser";
+import Login from "/src/pages/Login";
+import Join from "/src/pages/Join";
+import SocialJoin from "/src/pages/SocialJoin";
+import UserFind from "/src/pages/UserFind";
+import ResetPassword from "/src/pages/ResetPassword";
 import User from "/src/pages/User";
 
 /**
  * TODO:
- * - 메인 페이지
- * - 영화 페이지
- * - 시리즈 페이지
- * - 로그인 페이지
- * - 회원가입 페이지
- * - 영화/시리즈 상세 페이지
- * - 리뷰 작성 페이지
- * - 검색 결과 페이지
- * - 사용자 정보 수정 페이지
- * - 사용자 탈퇴 페이지
- * - 사용자 프로필 페이지
- * - 마이 페이지
+ * - 장르 홈
+ * - 배우 홈
+ * - 스태프 홈
  * - 404 페이지
  */
 
@@ -45,12 +39,25 @@ const MainRoutes = {
       element: <Content />,
     },
     {
-      path: "/user/profile",
-      element: <EditUser />,
+      path: "/user/login",
+      element: <Login />,
     },
     {
-      path: "/user/delete",
-      element: <DeleteUser />,
+      path: "/user/join",
+      element: <Join />,
+    },
+    {
+      path: "/user/auth/:provider",
+      // path: "auth/:provider/callback",
+      element: <SocialJoin />,
+    },
+    {
+      path: "/user/find/:type",
+      element: <UserFind />,
+    },
+    {
+      path: "/user/reset/password",
+      element: <ResetPassword />,
     },
     {
       path: "/user/:userId",
