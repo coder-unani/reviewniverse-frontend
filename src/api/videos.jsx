f6bf13cdfb4ac4fe494333d4a47ae0654b5c353f
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-16 22:47:02 +0900

    [refactor] videos.jsx 업데이트
    - endpoints 상수 추가
    - API url을 endpoints 상수 사용으로 수정
    - 비디오 상세, 리뷰, 내정보 API 추가

--- src/api/videos.jsx
+++ src/api/videos.jsx
@@ -2,6 +2,16 @@ import HttpClient from "/src/utils/HttpClient";
 import { settings } from "/src/config/settings";
 import { cLog, cError } from "/src/utils/test";
 
+const baseURL = settings.API_BASE_URL;
+const endpoints = {
+  videos: baseURL + "/v1/contents/videos",
+  videoDetail: baseURL + "/v1/contents/videos/:videoId",
+  videoReviews: baseURL + "/v1/contents/videos/:videoId/reviews",
+  videoMyInfo: baseURL + "/v1/contents/videos/:videoId/myinfo",
+  videoLike: baseURL + "/v1/contents/videos/:videoId/like",
+  videoRating: baseURL + "/v1/contents/videos/:videoId/ratings",
+};
+
 // Video List API
 /**
  * PARAMS:
@@ -28,7 +38,7 @@ export const fetchVideos = async ({
 }) => {
   try {
     const client = new HttpClient();
-    const res = await client.get(`${settings.API_BASE_URL}/v1/contents/videos`, {
+    const res = await client.get(endpoints.videos, {
       ...(page && { p: page }),
       ...(pageSize && { ps: pageSize }),
       ...(code && { cd: code }),
@@ -40,6 +50,59 @@ export const fetchVideos = async ({
       ...(orderBy && { ob: orderBy }),
     });
     return res.status === 200 ? res.data : [];
+  } catch (error) {
+    cError(error);
+  }
+};
+
+export const fetchVideoDetail = async ({ videoId }) => {
+  try {
+    const client = new HttpClient();
+    const res = await client.get(endpoints.videoDetail.replace(":videoId", videoId));
+    return res.status === 200 ? res.data : [];
+  } catch (error) {
+    cError(error);
+  }
+};
+
+export const fetchVideoReviews = async ({ videoId, page = null, pageSize = null }) => {
+  try {
+    const client = new HttpClient();
+    const res = await client.get(endpoints.videoReviews.replace(":videoId", videoId), {
+      ...(page && { p: page }),
+      ...(pageSize && { ps: pageSize }),
+    });
+    return res.status === 200 ? res.data : [];
+  } catch (error) {
+    cError(error);
+  }
+};
+
+export const fetchVideoMyInfo = async ({ videoId }) => {
+  try {
+    const client = new HttpClient();
+    const res = await client.post(endpoints.videoMyInfo.replace(":videoId", videoId));
+    return res.status === 200 ? res.data : [];
+  } catch (error) {
+    cError(error);
+  }
+};
+
+export const fetchVideoLike = async ({ videoId }) => {
+  try {
+    const client = new HttpClient();
+    const res = await client.post(endpoints.videoLike.replace(":videoId", videoId));
+    return res.status === 200 ? res.data : [];
+  } catch (error) {
+    cError(error);
+  }
+};
+
+export const fetchVideoRating = async ({ videoId, rating }) => {
+  try {
+    const client = new HttpClient();
+    const res = await client.post(endpoints.videoRating.replace(":videoId", videoId), {}, { rating });
+    return res.status === 204 ? res.code : "";
   } catch (error) {
     cError(error);
   }
