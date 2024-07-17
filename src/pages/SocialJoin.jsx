7ee3b258b87dbbef609165a84c00a073b57b914e
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-17 18:37:06 +0900

    [refactor] AuthContext의 user, signIn, signUp을 사용하도록 수정

--- src/pages/SocialJoin.jsx
+++ src/pages/SocialJoin.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import { useAuthContext } from "/src/context/AuthContext";
 import { useThemeContext } from "/src/context/ThemeContext";
-import HttpClient from "/src/utils/HttpClient";
 import { formatProvider } from "/src/utils/contentFormat";
 import { isEmpty } from "lodash";
 import JoinAgree from "/src/components/JoinAgree";
@@ -10,60 +10,64 @@ import Logo from "/assets/logo.svg";
 import "/src/styles/Join.css";
 import { cLog, cError } from "/src/utils/test";
 
-const API_BASE_URL = "https://comet.orbitcode.kr/v1";
-
 /**
  * TODO:
- * 1. 약관동의 받기
- * 2. 선택한 약관 동의 값, 사용자 정보 전달
- * 3. 회원가입 성공 시 모달창 띄우기
+ * - 성향 등록 추가
+ * - 회원가입 성공 시 모달창 띄우기
  */
 
 const SocialJoin = () => {
+  const navigate = useNavigate();
+  const { user, signUp, signIn } = useAuthContext();
+  const snsUser = JSON.parse(sessionStorage.getItem("sns_user"));
   const { isMobile } = useThemeContext();
   const { provider } = useParams();
-
   // 약관 동의 상태
   const [isAgree, setIsAgree] = useState(false);
   // 선택한 약관 동의 값
   const [agreeValues, setAgreeValues] = useState({});
 
+  const handleSocialJoin = async (user) => {
+    const res = await signUp(user);
+    if (res) {
+      cLog("회원가입에 성공했습니다.");
+
+      const signInUser = {
+        code: formatProvider(provider),
+        email: snsUser.email,
+        sns_id: snsUser.uid,
+      };
+
+      const signInRes = await signIn(signInUser);
+      if (signInRes) {
+        window.location.href = "/";
+      }
+    } else {
+      cLog("회원가입에 실패했습니다.");
+    }
+  };
+
+  useEffect(() => {
+    if (user) navigate("/");
+  }, []);
+
   useEffect(() => {
     // isAgree 값이 true 거나 agreeValues가 존재할 경우 회원가입 진행
-    if (!isAgree || isEmpty(agreeValues) || !provider) return;
+    if (!isAgree || isEmpty(agreeValues) || !provider || user || !snsUser) return;
 
-    // 회원가입 처리
-    try {
-      const user = JSON.parse(sessionStorage.getItem("sns_user"));
-      const client = new HttpClient();
-      client
-        .post(`${API_BASE_URL}/users/sns/signup`, {
-          code: formatProvider(provider),
-          email: user.email,
-          sns_id: user.uid,
-          nickname: user.displayName,
-          profile_image: user.photoURL,
-          is_privacy_agree: agreeValues.privacy,
-          is_terms_agree: agreeValues.terms,
-          is_age_agree: agreeValues.age,
-          is_marketing_agree: agreeValues.marketing,
-        })
-        .then((res) => {
-          if (res.status === 201) {
-            // res.code === "USER_CREATE_SUCC"
-            // TODO: 회원가입 성공 시 회원가입 성공 페이지로 이동
-            cLog("회원가입을 축하합니다.");
-            window.location.href = "/";
-            return;
-          } else {
-            // 실패
-            cLog("회원가입에 실패했습니다.");
-            return;
-          }
-        });
-    } catch (error) {
-      cError(error);
-    }
+    const signUpUser = {
+      code: formatProvider(provider),
+      email: snsUser.email,
+      sns_id: snsUser.uid,
+      nickname: snsUser.displayName,
+      profile_image: snsUser.photoURL,
+      is_privacy_agree: agreeValues.privacy,
+      is_terms_agree: agreeValues.terms,
+      is_age_agree: agreeValues.age,
+      is_marketing_agree: agreeValues.marketing,
+    };
+
+    handleSocialJoin(signUpUser);
   }, [isAgree, agreeValues, provider]);
 
   return (
