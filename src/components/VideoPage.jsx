26638a5cb790be72a60853584de781b902189745
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-18 23:27:56 +0900

    [fix] 렌더링 순서 수정

--- src/components/VideoPage.jsx
+++ src/components/VideoPage.jsx
@@ -6,11 +6,13 @@ const VideoPage = ({ screens, videos, handlePage }) => {
   return (
     <main className="main">
       {screens && screens.map((content, index) => <HVideos key={index} content={content} />)}
-      <Videos videos={videos} handlePage={handlePage}>
-        <div className="title-wrapper">
-          <h2 className="title">주인님 내 새끼 구경 좀 해봐요 🦦</h2>
-        </div>
-      </Videos>
+      {videos && (
+        <Videos videos={videos} handlePage={handlePage}>
+          <div className="title-wrapper">
+            <h2 className="title">주인님 내 새끼 구경 좀 해봐요 🦦</h2>
+          </div>
+        </Videos>
+      )}
     </main>
   );
 };
