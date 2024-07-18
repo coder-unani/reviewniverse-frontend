87ba664ae46594831efc0edd2d14f3edd8feac8c
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-19 02:20:48 +0900

    [refactor] 배우/스태프 이미지 PeopleImage 컴포넌트로 교체

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { LazyLoadImage } from "react-lazy-load-image-component";
-import "react-lazy-load-image-component/src/effects/blur.css";
-import { DEFAULT_IMAGES } from "/src/config/constants";
+import PeopleImage from "/src/components/Button/People/PeopleImage";
 
 const People = (props) => {
   const { crew, formatCode } = props;
 
   return (
     <Link to={`/people/${crew.id}`} state={{ people: crew }} className="crew" key={crew.id}>
-      <figure className="image">
-        <LazyLoadImage src={crew.picture || DEFAULT_IMAGES.noActor} alt={crew.name} effect="blur" />
-      </figure>
+      <PeopleImage image={crew.picture} size={54} />
       <div className="name-wrapper">
         <span className="name">{crew.name}</span>
         <div className="role">
