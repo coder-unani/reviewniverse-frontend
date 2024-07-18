3214e61aed4a4735c6f6125fad43d3f914f0e31d
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-18 22:55:31 +0900

    [feat] 비디오 검색 목록을 가져오기 위한 useVideoSearch hook 추가

--- src/hooks/useVideoSearch.jsx
+++ src/hooks/useVideoSearch.jsx
@@ -1,5 +1,49 @@
-/**
- * https://comet.orbitcode.kr/v1/contents/videos
- *
- * /pages/Search.jsx
- */
+import { useQuery } from "@tanstack/react-query";
+import { fetchVideoSearch } from "/src/api/videos";
+
+export const useVideoSearch = ({
+  query,
+  page = 1,
+  size = 20,
+  display = null,
+  mode = null,
+  target = null,
+  orderBy = null,
+  enabled = true,
+}) => {
+  const STALE_TIME = 1000 * 60 * 5;
+  const CACHE_TIME = 1000 * 60 * 10;
+  const RETRY = 1;
+  const queryKey = [
+    "videos",
+    {
+      query,
+      page,
+      size,
+      ...(display !== null && { display }),
+      ...(mode !== null && { mode }),
+      ...(target !== null && { target }),
+      ...(orderBy !== null && { orderBy }),
+    },
+  ];
+
+  return useQuery({
+    queryKey: queryKey,
+    queryFn: async () => {
+      const res = await fetchVideoSearch({
+        query,
+        page,
+        size,
+        display,
+        mode,
+        target,
+        orderBy,
+      });
+      return res;
+    },
+    enabled: !!enabled,
+    staleTime: STALE_TIME,
+    cacheTime: CACHE_TIME,
+    retry: RETRY,
+  });
+};
