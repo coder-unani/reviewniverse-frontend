f593cd381d0c6ed52f173c7d70d8b6f38e71e472
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-19 23:36:48 +0900

    [refactor] 구글 로그인 기능 수정, 네이버 로그인 기능 구현

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "/src/context/AuthContext";
 import { useThemeContext } from "/src/context/ThemeContext";
-import { getAuth, signInWithPopup } from "firebase/auth";
+import { signInWithPopup } from "firebase/auth";
 import { auth, provider } from "/src/auth/firebase";
 import BackButton from "/src/components/Button/Back";
 import Logo from "/assets/logo.svg";
@@ -40,43 +40,38 @@ const Login = () => {
       window.location.href = "/";
     } else {
       // 가입되어 있지 않다면 유저 정보를 가지고 회원가입 페이지로 이동
-      sessionStorage.setItem("sns_user", JSON.stringify(googleUser));
-      navigate("/user/auth/google");
+      const setUser = {
+        ...snsUser,
+        nickname: googleUser.displayName,
+        profile_image: googleUser.photoURL,
+      };
+      sessionStorage.setItem("sns_user", JSON.stringify(setUser));
+      navigate("/user/auth/google/callback");
     }
   };
 
   // TODO: 네이버 계정 연동 로그인 구현 (네이버 API 연동)
   const handleNaverLogin = () => {
-    window.location.href = "https://comet.orbitcode.kr/v1/users/naver/login";
-    // try {
-    //   const client = new HttpClient();
-    //   const res = client.get("http://0.0.0.0:8000/v1/users/naver/login");
-    //   if (res.status === 200) {
-    //     // 성공
-    //     const user = JSON.stringify(formatUser(res.data.user));
-    //     if (user && res.data.access_token && res.data.refresh_token) {
-    //       sessionStorage.setItem("user", user);
-    //       sessionStorage.setItem("access_token", res.data.access_token);
-    //       sessionStorage.setItem("refresh_token", res.data.refresh_token);
-    //       // TODO: 이전 페이지로 이동
-    //       window.location.href = "/";
-    //     }
-    //   } else if (res.status !== 200) {
-    //     // 실패
-    //     if (res.status === 400 || res.status === 401) {
-    //       // TODO: 에러 메시지 출력
-    //       cLog("로그인에 실패하였습니다.");
-    //     }
-    //     return;
-    //   }
-    // } catch (error) {
-    //   cError(error);
-    // }
+    const { naver } = window;
+    const naverLogin = new naver.LoginWithNaverId({
+      clientId: import.meta.env.VITE_NAVER_CLIENT_ID,
+      callbackUrl: import.meta.env.VITE_NAVER_CALLBACK_URL,
+      isPopup: false, // 팝업 방식으로 로그인 페이지가 열리도록 설정
+      loginButton: { color: "white", type: 1, height: 60 },
+    });
+
+    naverLogin.init();
+
+    // 실제 로그인 페이지로 리디렉션하는 부분
+    const loginButton = document.getElementById("naverIdLogin").firstChild;
+    if (loginButton) {
+      loginButton.click();
+    }
   };
 
   useEffect(() => {
     if (user) navigate("/");
-  }, []);
+  }, [user]);
 
   return (
     <div className="login-wrapper">
@@ -90,6 +85,7 @@ const Login = () => {
           <img src={KaKao} alt="kakao" />
           카카오로 시작하기
         </button>
+        <div id="naverIdLogin" style={{ display: "none" }} />
         <button type="button" className="naver" onClick={handleNaverLogin}>
           <img src={Naver} alt="naver" />
           네이버로 시작하기
