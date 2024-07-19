556e3d8f5e733755215949465542c81b6ffa8503
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-20 03:52:00 +0900

    [refactor] 세션스토리지 저장 함수 사용

--- src/auth/NaverCallback.jsx
+++ src/auth/NaverCallback.jsx
@@ -1,12 +1,13 @@
 import React, { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "/src/context/AuthContext";
+import { setSessionStorage } from "/src/utils/storage";
 import { cError } from "/src/utils/test";
-const { naver } = window;
 
 const NaverCallback = () => {
   const { signIn } = useAuthContext();
   const navigate = useNavigate();
+  const { naver } = window;
 
   useEffect(() => {
     const naverLogin = new naver.LoginWithNaverId({
@@ -14,7 +15,9 @@ const NaverCallback = () => {
       callbackUrl: import.meta.env.VITE_NAVER_CALLBACK_URL,
       isPopup: false,
     });
+
     naverLogin.init();
+
     naverLogin.getLoginStatus(async (status) => {
       if (status) {
         const naverUser = naverLogin.user;
@@ -29,13 +32,13 @@ const NaverCallback = () => {
         if (res) {
           window.location.href = "/";
         } else {
-          // 가입되어 있지 않다면 유저 정보를 가지고 회원가입 페이지로 이동
           const setUser = {
             ...snsUser,
             nickname: naverUser.nickname,
             profile_image: naverUser.profile_image,
           };
-          sessionStorage.setItem("sns_user", JSON.stringify(setUser));
+
+          setSessionStorage("sns_user", setUser);
           navigate("/user/auth/naver/callback");
         }
       } else {
