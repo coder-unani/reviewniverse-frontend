67d4802f05a10615d684e52a007a775b93329ae4
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-22 22:53:18 +0900

    [feat] Error 페이지 구현

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { DEFAULT_IMAGES } from "/src/config/constants";
+import "/src/styles/Error.css";
 
 const Error = () => {
-  return <div>Error</div>;
+  return (
+    <div className="error-main">
+      <section className="error-contents">
+        <div className="error">
+          <img src={DEFAULT_IMAGES.error} alt="페이지를 찾을 수 없음" />
+          <p className="title">서비스에 접속할 수 없습니다.</p>
+          <p className="sub-title">
+            죄송합니다. 기술적인 문제로 일시적으로 접속되지 않습니다.
+            <br />
+            잠시 후 다시 이용 부탁드립니다.
+          </p>
+          <Link to="/">리뷰니버스 홈</Link>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Error;
