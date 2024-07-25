d4fc73980b68041f129d1a0799d1403764963fcc
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-26 03:00:25 +0900

    [refactor] useVideoRating hook onSuccess 로직 수정

--- src/hooks/useVideoRating.jsx
+++ src/hooks/useVideoRating.jsx
@@ -7,33 +7,29 @@ export const useVideoRating = () => {
 
   return useMutation({
     mutationFn: async (variables) => await fetchVideoRating(variables),
-    onSuccess: (data, variables) => {
-      cLog("비디오 평가 점수가 변경되었습니다.");
-      // videoMyInfo 쿼리 키의 데이터를 업데이트
-      queryClient.setQueryData(["videoMyInfo", variables.videoId], (prev) => {
-        if (!prev) return prev;
-        const updatedRating =
-          data === "RATING_CREATE_SUCC" || data === "RATING_UPDATE_SUCC"
-            ? variables.rating
-            : data === "RATING_DELETE_SUCC"
-            ? 0
-            : prev.rating;
-        // review 데이터가 있는 경우
-        if (prev.review && prev.review.id) {
-          // videoReviews 쿼리 키의 데이터를 업데이트
-          queryClient.setQueryData(["videoReviews", variables.videoId], (prevReviews) => {
-            if (!prevReviews) return prevReviews;
-            const updatedReviews = prevReviews.map((review) =>
-              review.id === prev.review.id ? { ...review, rating: updatedRating === 0 ? null : updatedRating } : review
-            );
-            return updatedReviews;
-          });
-        }
-        return {
-          ...prev,
-          rating: updatedRating,
-        };
-      });
+    onSuccess: (res, variables) => {
+      if (res.status === 200) {
+        cLog("비디오 평가 점수가 변경되었습니다.");
+        // videoMyInfo 쿼리 키의 데이터를 업데이트
+        queryClient.setQueryData(["videoMyInfo", variables.videoId], (prevMyInfo) => {
+          if (!prevMyInfo) return prevMyInfo;
+          const updatedRating = res.data.data.rating;
+          // review 데이터가 있는 경우
+          if (prevMyInfo.review && prevMyInfo.review.id) {
+            // videoReviews 쿼리 키의 데이터를 업데이트
+            queryClient.setQueryData(["videoReviews", variables.videoId], (prevReviews) => {
+              if (!prevReviews) return prevReviews;
+              const updatedReviews = prevReviews.map((review) =>
+                review.id === prevMyInfo.review.id ? { ...review, rating: updatedRating } : review
+              );
+              return updatedReviews;
+            });
+          }
+          return { ...prevMyInfo, rating: updatedRating };
+        });
+      } else {
+        cLog("비디오 평가 점수 변경에 실패했습니다.");
+      }
     },
     onError: (error) => {
       cError(error);
