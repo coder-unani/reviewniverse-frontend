eddb0f9030555ce69b99643c7cfe72ee7016e9f4
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-25 23:44:47 +0900

    [refactor] 사용하지 않는 fetch 함수 삭제
    - fetchVideosSearch 를 fetchVideos 로 변경
    - fetchVideoDetailSearch 를 fetchVideoDetail 로 변경

--- src/api/videos.jsx
+++ src/api/videos.jsx
@@ -4,57 +4,12 @@ import { cLog, cError } from "/src/utils/test";
 
 const baseURL = settings.API_BASE_URL;
 const endpoints = {
-  videos: baseURL + "/v1/contents/videos",
-  videosSearch: baseURL + "/v1/videos",
-  videoDetail: baseURL + "/v1/contents/videos/:videoId",
-  videoDetailSearch: baseURL + "/v1/videos/:videoId",
+  videos: baseURL + "/v1/videos",
+  videoDetail: baseURL + "/v1/videos/:videoId",
   videoReviews: baseURL + "/v1/contents/videos/:videoId/reviews",
   videoMyInfo: baseURL + "/v1/contents/videos/:videoId/myinfo",
   videoLike: baseURL + "/v1/contents/videos/:videoId/like",
   videoRating: baseURL + "/v1/contents/videos/:videoId/ratings",
-};
-
-// Video List API
-/**
- * PARAMS:
- * - page: 페이지 번호
- * - pageSize: 한 번에 불러올 데이터 개수
- * - code: 컨텐츠 코드 10: Movies, 11: Series
- * - query: 검색어
- * - videoId: 비디오 ID
- * - actorId: 배우 ID
- * - staffId: 스태프 ID
- * - genreId: 장르 ID
- * - orderBy: 정렬 순서
- */
-export const fetchVideos = async ({
-  page = null,
-  pageSize = null,
-  code = null,
-  query = null,
-  videoId = null,
-  actorId = null,
-  staffId = null,
-  genreId = null,
-  orderBy = null,
-}) => {
-  try {
-    const client = new HttpClient();
-    const res = await client.get(endpoints.videos, {
-      ...(page && { p: page }),
-      ...(pageSize && { ps: pageSize }),
-      ...(code && { cd: code }),
-      ...(query && { q: query }),
-      ...(videoId && { vid: videoId }),
-      ...(actorId && { aid: actorId }),
-      ...(staffId && { sid: staffId }),
-      ...(genreId && { gid: genreId }),
-      ...(orderBy && { ob: orderBy }),
-    });
-    return res;
-  } catch (error) {
-    cError(error);
-  }
 };
 
 // Video List API
@@ -68,7 +23,7 @@ export const fetchVideos = async ({
  * - target: 검색 타겟
  * - orderBy: 정렬
  */
-export const fetchVideosSearch = async ({
+export const fetchVideos = async ({
   query = null,
   page = null,
   size = null,
@@ -79,7 +34,7 @@ export const fetchVideosSearch = async ({
 }) => {
   try {
     const client = new HttpClient();
-    const res = await client.get(endpoints.videosSearch, {
+    const res = await client.get(endpoints.videos, {
       ...(query && { q: query }),
       ...(page && { p: page }),
       ...(size && { s: size }),
@@ -98,16 +53,6 @@ export const fetchVideoDetail = async ({ videoId }) => {
   try {
     const client = new HttpClient();
     const res = await client.get(endpoints.videoDetail.replace(":videoId", videoId));
-    return res.status === 200 ? res.data : [];
-  } catch (error) {
-    cError(error);
-  }
-};
-
-export const fetchVideoDetailSearch = async ({ videoId }) => {
-  try {
-    const client = new HttpClient();
-    const res = await client.get(endpoints.videoDetailSearch.replace(":videoId", videoId));
     return res.status === 200 ? res.data : [];
     ç;
   } catch (error) {
