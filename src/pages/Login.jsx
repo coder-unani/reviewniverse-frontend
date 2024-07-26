977041bc16cc6ed0dadbf33790627be7bf6694bd
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-26 23:20:27 +0900

    [refactor] 구글 로그인 분리

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,56 +3,28 @@ import BackButton from "/src/components/Button/Back";
 import { useNavigate } from "react-router-dom";
 import { useAuthContext } from "/src/context/AuthContext";
 import { useThemeContext } from "/src/context/ThemeContext";
-import { signInWithPopup } from "firebase/auth";
-import { auth, provider } from "/src/auth/firebase";
-import { setStorageSnsUser } from "/src/utils/formatStorage";
 import { settings } from "/src/config/settings";
 import Logo from "/assets/logo.svg";
 import KaKao from "/assets/kakao.png";
 import Naver from "/assets/naver.png";
 import Google from "/assets/google.png";
 import "/src/styles/Login.css";
-import { cLog, cError } from "/src/utils/test";
 
 // TODO: authContext 사용하여 로그인 처리
 
 const Login = () => {
-  const { user, signIn } = useAuthContext();
+  const { user } = useAuthContext();
   const { isMobile } = useThemeContext();
   const navigate = useNavigate();
 
   // TODO: 카카오 계정 연동 로그인 구현 (카카오디펠롭퍼스 API 연동)
   const handleKakaoLogin = () => {
-    cLog("카카오 로그인");
+    window.location.href = `https://kauth.kakao.com/oauth/authorize?client_id=${settings.KAKAO_API_KEY}&redirect_uri=${settings.KAKAO_CALLBACK_URL}&response_type=code`;
   };
 
   // 구글 계정 연동 로그인 구현
-  const handleGoogleLogin = async () => {
-    const googleRes = await signInWithPopup(auth, provider);
-    const googleUser = googleRes.user;
-
-    const signInUser = {
-      code: "11",
-      email: googleUser.email,
-      sns_id: googleUser.uid,
-    };
-
-    const res = await signIn(signInUser);
-    if (res) {
-      window.location.href = "/";
-    } else {
-      // 가입되어 있지 않다면 유저 정보를 가지고 회원가입 페이지로 이동
-      const snsUser = {
-        code: "11",
-        email: googleUser.email,
-        sns_id: googleUser.uid,
-        nickname: googleUser.displayName,
-        profile_image: googleUser.photoURL,
-      };
-
-      setStorageSnsUser(snsUser);
-      navigate("/user/auth/google/callback");
-    }
+  const handleGoogleLogin = () => {
+    navigate("/user/auth/google/callback");
   };
 
   // 네이버 계정 연동 로그인 구현
@@ -83,7 +55,7 @@ const Login = () => {
       {isMobile && <BackButton />}
       <div className="login-header">
         <img src={Logo} className="logo" alt="logo" />
-        <h2>로그인</h2>
+        <h2>소셜 로그인</h2>
       </div>
       <div className="login-sns">
         <button type="button" className="kakao" onClick={handleKakaoLogin}>
