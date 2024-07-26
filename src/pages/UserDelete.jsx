e48ed739f976f47b085a2555ade28dc3be59c4fc
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-27 02:03:33 +0900

    [refactor] 취소 버튼 추가, 버튼 레이아웃 변경

--- src/pages/UserDelete.jsx
+++ src/pages/UserDelete.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { DEFAULT_IMAGES } from "/src/config/constants";
 import "/src/styles/UserDelete.css";
 import { cLog } from "/src/utils/test";
 
 const UserDelete = () => {
+  const navigate = useNavigate();
+
+  const handleCancel = () => {
+    // 이전 페이지로 이동
+    navigate(-1);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
     cLog("탈퇴하기");
@@ -20,7 +28,14 @@ const UserDelete = () => {
             삭제하시려면 아래의 버튼을 눌러주세요.
           </p>
           <img src={DEFAULT_IMAGES.userDelete} alt="회원 탈퇴 이미지" />
-          <button type="submit">탈퇴 하기</button>
+          <div className="button-wrapper">
+            <button type="button" className="cancel" onClick={handleCancel}>
+              안할래요!
+            </button>
+            <button type="button" className="delete">
+              탈퇴하기
+            </button>
+          </div>
         </form>
       </div>
     </main>
