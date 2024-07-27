9e8dd0ce65d1eca1f2b89b1525dd15d40ef50ee9
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-28 02:10:41 +0900

    [refactor] fetchToken return 값 수정

--- src/api/token.jsx
+++ src/api/token.jsx
@@ -11,7 +11,7 @@ export const fetchToken = async () => {
   try {
     const client = new HttpClient();
     const res = await client.get(endpoints.token);
-    return res.status === 200 ? true : false;
+    return res;
   } catch (error) {
     cError(error);
   }
