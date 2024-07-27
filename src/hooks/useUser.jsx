2a1c9e7327d42d1cbc716feeb1c3f0800a076823
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-27 23:20:50 +0900

    [refactor] fetch 함수 변경 및 staleTime, cacheTime 옵션 삭제

--- src/hooks/useUser.jsx
+++ src/hooks/useUser.jsx
@@ -1,22 +1,15 @@
 import { useQuery } from "@tanstack/react-query";
-// import { fetchUser } from "/src/api/users";
-import { fetchUsers } from "/src/api/users";
+import { fetchUser } from "/src/api/users";
 
 export const useUser = ({ userId }) => {
   return useQuery({
     queryKey: ["user", userId],
     queryFn: async () => {
-      const res = await fetchUsers({ userId });
-      if (res.status === 200) {
-        return res.data.data[0];
-        // return res.data.user;
-      } else {
-        throw new Error("사용자 정보를 불러오는데 실패하였습니다.");
-      }
+      const res = await fetchUser({ userId });
+      return res.status === 200 ? res.data.user : null;
     },
-    // TODO: staleTime, cacheTime 변경 (staleTime: 5분, cacheTime: 10분)
-    staleTime: 1000 * 60 * 5,
-    cacheTime: 1000 * 60 * 10,
+    // staleTime: 1000 * 60 * 5,
+    // cacheTime: 1000 * 60 * 10,
     retry: 1,
   });
 };
