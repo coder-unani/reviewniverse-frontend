81421a7f3680e9ad6f9c18cfcd0804aab6f5023a
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-29 13:21:34 +0900

    [feat] 닉네임 유효성 검증을 위한 useValidateNickname hook 구현

---
+++ src/hooks/useValidateNickname.jsx
@@ -0,0 +1,31 @@
+import { fetchValidateNickname } from "/src/api/users";
+import { cLog, cError } from "/src/utils/test";
+
+export const useValidateNickname = async ({ nickname }) => {
+  try {
+    const res = await fetchValidateNickname({ nickname });
+    if (res.status === 200) {
+      if (res.data) {
+        return {
+          status: res.data,
+          code: "",
+        };
+      } else {
+        return {
+          status: res.data,
+          code: "이미 사용중인 닉네임입니다.",
+        };
+      }
+    } else {
+      return {
+        status: false,
+        code: "닉네임 중복검사에 실패했습니다.",
+      };
+    }
+  } catch (error) {
+    return {
+      status: false,
+      code: "닉네임 중복검사에 실패했습니다.",
+    };
+  }
+};
