0010b3a4d9aefe23a513d2c8d16b6bc12225f4dd
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-29 13:22:43 +0900

    [refactor] mutation 사용 안함, validation 추가 및 return 값 수정

--- src/hooks/useUser.jsx
+++ src/hooks/useUser.jsx
@@ -1,24 +1,41 @@
-import { useNavigate } from "react-router-dom";
-import { useMutation } from "@tanstack/react-query";
 import { fetchUser } from "/src/api/users";
+import { cLog, cError } from "/src/utils/test";
 
-export const useUser = ({ userId }) => {
-  const navigate = useNavigate();
-
-  return useMutation({
-    mutationFn: async () => {
-      const res = await fetchUser({ userId });
-      if (res.status === 200) {
-        return res.data.user;
-      } else if (res.status === 404) {
-        if (res.message.detail === "USER_NOT_FOUND") {
-          return null;
-        } else {
-          navigate("/error");
-        }
+export const useUser = async ({ userId }) => {
+  try {
+    const res = await fetchUser({ userId });
+    if (res.status === 200) {
+      return {
+        status: true,
+        code: "",
+        data: res.data.user,
+      };
+    } else if (res.status === 404) {
+      if (res.message.detail === "USER_NOT_FOUND") {
+        return {
+          status: false,
+          code: "탈퇴한 회원입니다.",
+          data: null,
+        };
       } else {
-        navigate("/error");
+        return {
+          status: false,
+          code: "회원정보를 가져오는데 실패했습니다.",
+          data: null,
+        };
       }
-    },
-  });
+    } else {
+      return {
+        status: false,
+        code: "회원정보를 가져오는데 실패했습니다.",
+        data: null,
+      };
+    }
+  } catch (error) {
+    return {
+      status: false,
+      code: "회원정보를 가져오는데 실패했습니다.",
+      data: null,
+    };
+  }
 };
