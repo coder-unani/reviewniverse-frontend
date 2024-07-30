73688e1633e1201a4347dee68fca021250a5acc1
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-30 11:10:26 +0900

    [feat] 알림 표시를 위한 Toast.jsx 추가

---
+++ src/components/Toast.jsx
@@ -0,0 +1,35 @@
+import { ToastContainer, toast, Slide } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+export const showToast = (message) => {
+  toast(message);
+};
+
+export const showInfoToast = (message) => {
+  toast.info(message);
+};
+
+export const showSuccessToast = (message) => {
+  toast.success(message);
+};
+
+export const showWarningToast = (message) => {
+  toast.warn(message);
+};
+
+export const showErrorToast = (message) => {
+  toast.error(message);
+};
+
+export const ToastWrapper = () => (
+  <ToastContainer
+    position="top-center"
+    autoClose={2000}
+    hideProgressBar={true}
+    pauseOnFocusLoss={false}
+    theme="light"
+    // theme="colored"
+    transition={Slide}
+    limit={1}
+  />
+);
