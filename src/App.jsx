5b60d0104c01be508d3b3a2481a9b18d79fb3b17
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-31 23:56:28 +0900

    [refactor] 공통 meta 태그 App.jsx에 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { HelmetProvider } from "react-helmet-async";
+import { HelmetProvider, Helmet } from "react-helmet-async";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeContextProvider } from "/src/context/ThemeContext";
 import { RouterProvider } from "react-router-dom";
 import router from "/src/routes";
 import { ToastWrapper } from "/src/components/Toast";
+import { DEFAULT_IMAGES } from "/src/config/constants";
+import { SETTINGS } from "/src/config/settings";
 import "/src/styles/App.css";
 
 /**
@@ -19,6 +21,20 @@ function App() {
     <HelmetProvider>
       <QueryClientProvider client={queryClient}>
         <ThemeContextProvider>
+          <Helmet>
+            <title>리뷰니버스</title>
+            <meta name="description" content="리뷰니버스와 함께라면 보는 즐거움이 2배로, 생생한 리뷰를 확인해보세요!" />
+            <meta property="og:title" content="리뷰니버스" />
+            <meta
+              property="og:description"
+              content="리뷰니버스와 함께라면 보는 즐거움이 2배로, 생생한 리뷰를 확인해보세요!"
+            />
+            <meta property="og:image" content={DEFAULT_IMAGES.logo} />
+            <meta property="og:url" content={SETTINGS.DOMAIN_URL} />
+            <meta property="og:type" content="website" />
+            <meta property="og:locale" content="ko_KR" />
+            <meta property="og:site_name" content="Reviewniverse" />
+          </Helmet>
           <RouterProvider router={router} />
           <ToastWrapper />
         </ThemeContextProvider>
