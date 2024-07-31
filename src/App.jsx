0520fb7f6606aaeab961a5d34232cbede41201dc
hwakk <97441379+hwakk@users.noreply.github.com>
2024-07-31 13:37:28 +0900

    [refactor] HelmetProvider 추가 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { HelmetProvider } from "react-helmet-async";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ThemeContextProvider } from "/src/context/ThemeContext";
 import { RouterProvider } from "react-router-dom";
@@ -8,24 +9,21 @@ import "/src/styles/App.css";
 
 /**
  * TODO:
- * 0. 페이지 라우팅
- * 4-1. 아이디, 비밀번호 입력
- * 4-2. 로그인 버튼 클릭 시 로그인 처리
- * 4-3. 로그인 성공 시 메인 페이지로 이동
  * 4-4. 로그인 n회 이상 실패 시 사람인지 확인
- * 5-1. 아이디, 비밀번호, 비밀번호 확인, 이름, 이메일 입력
  */
 
 const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <ThemeContextProvider>
-        <RouterProvider router={router} />
-        <ToastWrapper />
-      </ThemeContextProvider>
-    </QueryClientProvider>
+    <HelmetProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeContextProvider>
+          <RouterProvider router={router} />
+          <ToastWrapper />
+        </ThemeContextProvider>
+      </QueryClientProvider>
+    </HelmetProvider>
   );
 }
 
