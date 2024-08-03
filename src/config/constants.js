6c57d04af8349e9c6d6181538aafe10ab70a89a3
hwakk <97441379+hwakk@users.noreply.github.com>
2024-08-04 01:46:25 +0900

    [refactor] 기본 아이콘 상수 설정 및 평점 문구 추가

--- src/config/constants.js
+++ src/config/constants.js
@@ -12,6 +12,25 @@ export const DEFAULT_IMAGES = {
   userDelete: "/assets/user-delete.png",
 };
 
+export const DEFAULT_ICONS = {
+  outlineFolder: "/assets/button/outline-folder.svg",
+  outlineHeart: "/assets/button/outline-heart.svg",
+  outlinePen: "/assets/button/outline-pen.svg",
+  outlineTrash: "/assets/button/outline-trash.svg",
+  outlineUpdate: "/assets/button/outline-update.svg",
+  outlineThumb1: "/assets/button/outline-thumb-1.svg",
+  outlineThumb2: "/assets/button/outline-thumb-2.svg",
+  fillFolder: "/assets/button/fill-folder.svg",
+  fillHeart: "/assets/button/fill-heart.svg",
+  fillPen: "/assets/button/fill-pen.svg",
+  fillTrash: "/assets/button/fill-trash.svg",
+  fillUpdate: "/assets/button/fill-update.svg",
+  fillThumb1: "/assets/button/fill-thumb-1.svg",
+  fillThumb2: "/assets/button/fill-thumb-2.svg",
+  arrowLeft: "/assets/button/arrow-left.svg",
+  arrowRight: "/assets/button/arrow-right.svg",
+};
+
 export const VIDEO_ORDER_OPTIONS = [
   "new_desc",
   "view_desc",
@@ -20,8 +39,8 @@ export const VIDEO_ORDER_OPTIONS = [
   "rating_desc",
   "release_desc",
 ];
-1;
-export const VIDEO_RATING_TEXT = ["최악이에요!", "싫어요", "그저그래요", "좋아요", "최고에요!"];
+
+export const VIDEO_RATING_TEXT = ["어떠셨나요?", "최악이에요!", "싫어요", "그저그래요", "좋아요", "최고에요!"];
 
 export const PROFILE_IMAGE_FILE_SIZE = 30;
 
