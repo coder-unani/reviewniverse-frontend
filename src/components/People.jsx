59710ff7777c3b4ac9b1c5eb86d0cc61dc6cbc14
hwakk <97441379+hwakk@users.noreply.github.com>
2024-08-04 01:50:35 +0900

    [refactor] 선택자 수정, 이미지 사이즈 수정

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -4,8 +4,8 @@ import PeopleImage from "/src/components/Button/People/PeopleImage";
 
 const People = ({ crew, target, formatCode }) => {
   return (
-    <Link to={`/people/${crew.id}`} state={{ people: crew, target }} className="crew" key={crew.id}>
-      <PeopleImage image={crew.picture} size={54} />
+    <Link to={`/people/${crew.id}`} state={{ people: crew, target }} className="cast" key={crew.id}>
+      <PeopleImage image={crew.picture} size={60} />
       <div className="name-wrapper">
         <span className="name">{crew.name}</span>
         <div className="role">
