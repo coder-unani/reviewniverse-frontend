34cb3c25bdf23d80e6cc01dbcd9544d8fb56e218
hwakk <97441379+hwakk@users.noreply.github.com>
2024-08-04 01:45:47 +0900

    [refactor] watch type 이미지 경로 수정

--- src/config/codes.js
+++ src/config/codes.js
@@ -16,37 +16,37 @@ export const USER_WATCH_TYPE = [
     id: 10,
     title: "소름돋는 연기력",
     subtitle: "언빌리버블",
-    image: "/assets/watchtype-1.png",
+    image: "/assets/watchtype/1.png",
   },
   {
     id: 11,
     title: "화려한 영상미",
     subtitle: "본격 눈호강",
-    image: "/assets/watchtype-2.png",
+    image: "/assets/watchtype/2.png",
   },
   {
     id: 12,
     title: "탄탄한 스토리",
     subtitle: "저게 저렇게 된다고?",
-    image: "/assets/watchtype-1.png",
+    image: "/assets/watchtype/1.png",
   },
   {
     id: 13,
     title: "감독연출",
     subtitle: "기가맥힘",
-    image: "/assets/watchtype-1.png",
+    image: "/assets/watchtype/1.png",
   },
   {
     id: 14,
     title: "고막 녹는 OST",
     subtitle: "본격 귀호강",
-    image: "/assets/watchtype-1.png",
+    image: "/assets/watchtype/1.png",
   },
   {
     id: 15,
     title: "초호화 캐스팅",
     subtitle: "걔도?쟤도?",
-    image: "/assets/watchtype-1.png",
+    image: "/assets/watchtype/1.png",
   },
 ];
 
