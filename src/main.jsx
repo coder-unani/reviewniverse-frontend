7bf4ac6e5d0d33c08ca08da5680cb7ba6710b26a
hwakk <97441379+hwakk@users.noreply.github.com>
2024-08-07 04:03:26 +0900

    [style] partial 파일 Main.scss에 import하여 사용, main.jsx에 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "/src/App.jsx";
-import "/src/styles/index.css";
+import "/src/styles/Main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
