9d8ac00eea912131e5b09014c359f9cb71ba7096
hwakk <97441379+hwakk@users.noreply.github.com>
2024-08-09 01:58:40 +0900

    [refactor] 스와이퍼 버튼 swiper 내장 기능인 slidePrev, slideNext 사용

--- src/components/HVideos.jsx
+++ src/components/HVideos.jsx
@@ -1,24 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import VideoRankItem from "/src/components/VideoRankItem";
 import VideoItem from "/src/components/VideoItem";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation } from "swiper/modules";
+import { Navigation, Virtual } from "swiper/modules";
 import "swiper/css";
 import ArrowLeftIcon from "/src/assets/button/arrow-left.svg?react";
 import ArrowRightIcon from "/src/assets/button/arrow-right.svg?react";
 import { isEmpty } from "lodash";
 
-const HVideos = ({ children, content, template, code }) => {
+const HVideos = ({ children, content, template }) => {
   const [videos, setVideos] = useState([]);
+  const [isBeginning, setIsBeginning] = useState(true);
+  const [isEnd, setIsEnd] = useState(false);
+  const swiperRef = useRef(null);
 
-  // 스와이퍼 설정
-  const swiperConfig = (prevEl, nextEl) => ({
-    modules: [Navigation],
+  const swiperConfig = {
+    modules: [Navigation, Virtual],
     spaceBetween: 8,
     slidesPerView: 3.01,
     slidesPerGroup: 3,
     speed: 1000,
-    navigation: { prevEl, nextEl },
+    virtual: true,
     allowTouchMove: true,
     breakpoints: {
       577: {
@@ -38,7 +40,16 @@ const HVideos = ({ children, content, template, code }) => {
         allowTouchMove: false,
       },
     },
-  });
+    onSwiper: (swiper) => {
+      swiperRef.current = swiper;
+      setIsBeginning(swiper.isBeginning);
+      setIsEnd(swiper.isEnd);
+    },
+    onSlideChange: (swiper) => {
+      setIsBeginning(swiper.isBeginning);
+      setIsEnd(swiper.isEnd);
+    },
+  };
 
   useEffect(() => {
     if (!content) return;
@@ -51,25 +62,25 @@ const HVideos = ({ children, content, template, code }) => {
     <section className="horizontal-videos-wrapper">
       {children}
       <div className="swiper-container">
-        <Swiper {...swiperConfig(`.prev-${code}`, `.next-${code}`)}>
+        <Swiper {...swiperConfig}>
           {template === "default"
             ? videos.map((video, index) => (
-                <SwiperSlide key={index}>
+                <SwiperSlide key={index} virtualIndex={index}>
                   <VideoItem video={video} index={index} />
                 </SwiperSlide>
               ))
             : videos.map((video, index) => (
-                <SwiperSlide key={index}>
+                <SwiperSlide key={index} virtualIndex={index}>
                   <VideoRankItem video={video} index={index} />
                 </SwiperSlide>
               ))}
         </Swiper>
-        <div className={`swiper-button-prev prev-${code}`}>
+        <button onClick={() => swiperRef.current.slidePrev()} className="swiper-button-prev" disabled={isBeginning}>
           <ArrowLeftIcon />
-        </div>
-        <div className={`swiper-button-next next-${code}`}>
+        </button>
+        <button onClick={() => swiperRef.current.slideNext()} className="swiper-button-next" disabled={isEnd}>
           <ArrowRightIcon />
-        </div>
+        </button>
       </div>
     </section>
   );
