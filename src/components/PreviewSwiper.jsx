0695d15d2453683c1af2f1eed7bc796b25e79dbd
hwakk <97441379+hwakk@users.noreply.github.com>
2024-08-09 12:10:54 +0900

    [feat] 프리뷰 스와이퍼 컴포넌트 분리

---
+++ src/components/PreviewSwiper.jsx
@@ -0,0 +1,105 @@
+import React, { useState, useEffect, useRef } from "react";
+import { Link } from "react-router-dom";
+import { formatDate } from "/src/utils/format";
+import { formatPreviewThumbnail, formatBackgroundImage, formatReleaseText } from "/src/utils/formatContent";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay } from "swiper/modules";
+import "swiper/css";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
+
+const PreviewSwiper = React.memo(({ screensMA01 }) => {
+  const [previewVideo, setPreviewVideo] = useState("");
+  const swiperRef = useRef(null);
+
+  const previewSwiperConfig = {
+    modules: [Autoplay],
+    spaceBetween: 10,
+    slidesPerView: "auto",
+    speed: 1000,
+    autoplay: {
+      delay: 6000,
+      disableOnInteraction: false,
+    },
+    loop: true,
+    allowTouchMove: true,
+    breakpoints: {
+      577: {
+        spaceBetween: 12,
+      },
+      769: {
+        spaceBetween: 18,
+      },
+      1281: {
+        spaceBetween: 24,
+      },
+    },
+    onSwiper: (swiper) => {
+      swiperRef.current = swiper;
+    },
+    onSlideChange: (swiper) => {
+      if (screensMA01 && screensMA01.content.list.length > 0) {
+        const activeIndex = swiper.realIndex;
+        setPreviewVideo(screensMA01.content.list[activeIndex]);
+      }
+    },
+  };
+
+  useEffect(() => {
+    if (screensMA01 && screensMA01.content.list.length > 0) {
+      setPreviewVideo(screensMA01.content.list[0]);
+    }
+  }, [screensMA01]);
+
+  return (
+    <>
+      <figure className="background-image">
+        {previewVideo && (
+          <LazyLoadImage src={formatPreviewThumbnail(previewVideo.thumbnail)} alt="배경 이미지" effect="blur" />
+        )}
+      </figure>
+
+      <div className="preview-info-wrapper">
+        {previewVideo && (
+          <div className="preview-info">
+            <div className="info-right">
+              <div className="info-title">
+                <p className="title-og">{previewVideo.title_og}</p>
+                <h2 className="title-kr">{previewVideo.title}</h2>
+              </div>
+            </div>
+            <div className="info-left">
+              <div className="info-release">
+                <span>{formatReleaseText(previewVideo.code)}</span>
+                <span>|</span>
+                <span>{formatDate(previewVideo.release)}</span>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+
+      <div className="preview-videos-wrapper">
+        {screensMA01 && (
+          <div className="preview-videos">
+            <Swiper {...previewSwiperConfig} className="preview">
+              {screensMA01.content.list.map((video, index) => (
+                <SwiperSlide key={index}>
+                  {({ isActive }) => (
+                    <Link to={`/contents/${video.id}`}>
+                      <figure className={`thumbnail ${isActive ? "active" : ""}`}>
+                        <LazyLoadImage src={formatBackgroundImage(video.thumbnail)} alt={video.title} effect="blur" />
+                      </figure>
+                    </Link>
+                  )}
+                </SwiperSlide>
+              ))}
+            </Swiper>
+          </div>
+        )}
+      </div>
+    </>
+  );
+});
+
+export default PreviewSwiper;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,21 +1,15 @@
-import React, { useEffect, useState, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import PreviewSwiper from "/src/components/PreviewSwiper";
 import HVideos from "/src/components/HVideos";
 import Videos from "/src/components/Videos";
-import { useRankingVideos } from "/src/hooks/useRankingVideos";
 import { useScreenVideos } from "/src/hooks/useScreenVideos";
+import { useRankingVideos } from "/src/hooks/useRankingVideos";
 import { useVideos } from "/src/hooks/useVideos";
-import { SCREEN_MAIN_ID } from "/src/config/codes";
-import { VIDEO_ORDER_OPTIONS } from "/src/config/constants";
-import { formatDate, arrayRandomValue } from "/src/utils/format";
-import { formatReleaseText } from "/src/utils/formatContent";
+import { SCREEN_PREVIEW_ID, SCREEN_MAIN_ID } from "/src/config/codes";
+// import { VIDEO_ORDER_OPTIONS } from "/src/config/constants";
 import { isEmpty } from "lodash";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules";
-import "swiper/css";
-import { LazyLoadImage } from "react-lazy-load-image-component";
-import "react-lazy-load-image-component/src/effects/blur.css";
-import ArrowRightIcon from "/src/assets/button/arrow-right.svg?react";
+// import ArrowRightIcon from "/src/assets/button/arrow-right.svg?react";
 
 const Home = () => {
   const navigate = useNavigate();
@@ -30,6 +24,11 @@ const Home = () => {
   } = useRankingVideos({ code: "20240808", count: 20 });
   const { data: screens, error: screensError, isLoading: screensIsLoading } = useScreenVideos({ code: SCREEN_MAIN_ID });
   const {
+    data: previews,
+    error: previewsError,
+    isLoading: previewsIsLoading,
+  } = useScreenVideos({ code: SCREEN_PREVIEW_ID, display: "detail" });
+  const {
     data: videosData,
     error: videosError,
     isLoading: videosIsLoading,
@@ -43,51 +42,11 @@ const Home = () => {
   const [screensMA03, setScreensMA03] = useState(null);
   const [screensMA04, setScreensMA04] = useState(null);
   const [screensMA05, setScreensMA05] = useState(null);
-  const [previewVideo, setPreviewVideo] = useState("");
-  const swiperRef = useRef(null);
-
-  // 프리뷰 비디오 스와이퍼 설정
-  const previewSwiperConfig = {
-    modules: [Autoplay],
-    spaceBetween: 10,
-    slidesPerView: "auto",
-    speed: 1000,
-    autoplay: {
-      delay: 6000,
-      disableOnInteraction: false,
-    },
-    loop: true,
-    allowTouchMove: true,
-    breakpoints: {
-      577: {
-        spaceBetween: 12,
-      },
-      769: {
-        spaceBetween: 18,
-      },
-      1281: {
-        spaceBetween: 24,
-      },
-    },
-    onSwiper: (swiper) => {
-      swiperRef.current = swiper;
-    },
-    onSlideChange: (swiper) => {
-      if (screensMA01 && screensMA01.content.list.length > 0) {
-        const activeIndex = swiper.realIndex;
-        setPreviewVideo(screensMA01.content.list[activeIndex]);
-      }
-    },
-    onSlideChangeStart: (swiper) => {
-      console.log(swiper.realIndex);
-    },
-  };
 
   const formatScreens = (screens, code) => {
     return screens.find((screen) => screen.code === code);
   };
 
-  // 페이지 변경
   const handlePage = (page) => {
     setPage(page);
   };
@@ -112,20 +71,15 @@ const Home = () => {
     };
   }, []);
 
+  // TODO: 정리 필요
   useEffect(() => {
-    if (screensMA01 && screensMA01.content.list.length > 0) {
-      setPreviewVideo(screensMA01.content.list[0]);
-    }
-  }, [screensMA01]);
-
-  useEffect(() => {
-    if (isEmpty(screens)) return;
-    setScreensMA01(formatScreens(screens, "MA01"));
+    if (isEmpty(previews) || isEmpty(screens) || isEmpty(previews)) return;
+    setScreensMA01(formatScreens(previews, SCREEN_PREVIEW_ID));
     setScreensMA02(formatScreens(screens, "MA02"));
     setScreensMA03(formatScreens(screens, "MA03"));
     setScreensMA04(formatScreens(screens, "MA04"));
     setScreensMA05(formatScreens(screens, "MA05"));
-  }, [screens]);
+  }, [previews, screens]);
 
   useEffect(() => {
     if (!videosData || !hasMore) return;
@@ -145,64 +99,22 @@ const Home = () => {
   }, [videosData, hasMore, page]);
 
   // 로딩중일때 표시할 화면 (스켈레톤 UI)
-  // if (videosIsLoading || rankingIsLoading || screensIsLoading) return null;
-  if (rankingIsLoading || screensIsLoading) return;
+  // if (previewsIsLoading || screensIsLoading || rankingIsLoading || videosIsLoading) return null;
+  if (previewsIsLoading || screensIsLoading || rankingIsLoading) return;
 
-  // 에러일때 표시할 화면
-  if (videosError || rankingError || screensError) return navigate("/error");
+  if (previewsError || screensError || rankingError || videosError) return navigate("/error");
 
-  // 데이터 props로 하위 컴포넌트에 전달
   return (
     <main className="home-main">
       <section className="home-preview-wrapper">
-        <figure className="background-image">
-          {previewVideo && <LazyLoadImage src={previewVideo.thumbnail} alt="배경 이미지" effect="blur" />}
-        </figure>
-
-        <div className="preview-info-wrapper">
-          {previewVideo && (
-            <div className="preview-info">
-              <div className="info-right">
-                <div className="info-title">
-                  {/* <p className="title-og">{previewVideo.title}</p> */}
-                  <h2 className="title-kr">{previewVideo.title}</h2>
-                </div>
-              </div>
-              <div className="info-left">
-                <div className="info-release">
-                  <span>{formatReleaseText(previewVideo.code)}</span>
-                  <span>|</span>
-                  <span>{formatDate(previewVideo.release)}</span>
-                </div>
-              </div>
-            </div>
-          )}
-        </div>
-
-        <div className="preview-videos-wrapper">
-          {screensMA01 && (
-            <div className="preview-videos">
-              <Swiper {...previewSwiperConfig} className="preview">
-                {screensMA01.content.list.map((video) => (
-                  <SwiperSlide key={video.id}>
-                    {({ isActive }) => (
-                      <figure className={`thumbnail ${isActive ? "active" : ""}`}>
-                        <LazyLoadImage src={video.thumbnail} alt={video.title} effect="blur" />
-                      </figure>
-                    )}
-                  </SwiperSlide>
-                ))}
-              </Swiper>
-            </div>
-          )}
-        </div>
+        <PreviewSwiper screensMA01={screensMA01} />
       </section>
 
       <section className="home-main-wrapper">
-        {screensMA01 && (
-          <HVideos content={screensMA01.content.list} template={screensMA01.content.template}>
+        {screensMA02 && (
+          <HVideos content={screensMA02.content.list} template={screensMA02.content.template}>
             <div className="title-wrapper">
-              <h2 className="title">{screensMA01.title}</h2>
+              <h2 className="title">{screensMA02.title}</h2>
               {/* <button className="more">
                   더보기
                   <ArrowRightIcon />
@@ -215,14 +127,6 @@ const Home = () => {
           <HVideos content={ranking} template="rank" code="ranking">
             <div className="title-wrapper">
               <h2 className="title">🍿 리뷰니버스 TOP 20</h2>
-            </div>
-          </HVideos>
-        )}
-
-        {screensMA02 && (
-          <HVideos content={screensMA02.content.list} template={screensMA02.content.template}>
-            <div className="title-wrapper">
-              <h2 className="title">{screensMA02.title}</h2>
             </div>
           </HVideos>
         )}
