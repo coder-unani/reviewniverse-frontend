1b04cd54b4c3e7e9dccce964433818ccc3aadcc6
hwakk <97441379+hwakk@users.noreply.github.com>
2024-08-18 03:23:40 +0900

    [refactor] 코드 스플리팅 적용

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,27 @@ export default defineConfig({
   build: {
     outDir: "dist",
     emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        // node_modules 내의 모듈을 청크로 분리
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id.toString().split("node_modules/")[1].split("/")[0].toString();
+          }
+        },
+        // 청크 파일 이름을 'chunk-[hash].js'로 지정
+        chunkFileNames: "js/chunk-[hash].js",
+        // 엔트리 파일 이름을 'entry-[hash].js'로 지정
+        entryFileNames: "js/entry-[hash].js",
+        // CSS 파일 이름을 'style-[hash].css'로 지정
+        assetFileNames: ({ name }) => {
+          if (name && name.endsWith(".css")) {
+            return "css/style-[hash][extname]";
+          }
+          return "assets/[name]-[hash][extname]";
+        },
+      },
+    },
   },
   resolve: {
     alias: [
