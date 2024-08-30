6c23e730308b4df00f33ea36eeff1c0631454afc
hwakk <97441379+hwakk@users.noreply.github.com>
2024-08-30 17:53:15 +0900

    [refactor] react-use-gesture, react-spring 라이브러리를 사용하여 터치, 스크롤 이벤트 제어

--- src/components/SwiperPreview.jsx
+++ src/components/SwiperPreview.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { useThemeContext } from "/src/context/ThemeContext";
+import { useDrag } from "@use-gesture/react";
+import { useSpring, animated } from "@react-spring/web";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Thumbs, Autoplay, FreeMode, Parallax, EffectFade } from "swiper/modules";
 import { LazyLoadImage } from "react-lazy-load-image-component";
@@ -17,6 +19,7 @@ const SwiperPreview = React.memo(({ screensMA01 }) => {
   const [previewVideo, setPreviewVideo] = useState("");
   const [thumbsSwiper, setThumbsSwiper] = useState(null);
   const [activeThumbIndex, setActiveThumbIndex] = useState(0);
+  const [isDragging, setIsDragging] = useState(false);
 
   // 메인 슬라이더 설정
   const mainSwiperConfig = {
@@ -34,7 +37,7 @@ const SwiperPreview = React.memo(({ screensMA01 }) => {
     onSlideChange: (swiper) => {
       setActiveThumbIndex(swiper.realIndex);
 
-      // 활성화된 슬라이드가 썸네일 슬라이더의 맨 앞으로 오도록 설정
+      // 모바일에서 활성화된 슬라이드가 썸네일 슬라이더의 맨 앞으로 오도록 설정
       if (!thumbsSwiper || !isMobile) {
         return;
       }
@@ -79,24 +82,49 @@ const SwiperPreview = React.memo(({ screensMA01 }) => {
     setPreviewVideo(screensMA01.content.list);
   }, [screensMA01]);
 
+  const [{ x }, api] = useSpring(() => ({ x: 0 }));
+
+  const bind = useDrag(
+    ({ down, movement: [mx], cancel, tap, args: [videoId, index] }) => {
+      if (tap) {
+        // 터치가 짧게 이루어진 경우 (클릭으로 간주)
+        handleLinkClick(videoId, index);
+      } else if (down) {
+        // 터치 중 스와이프가 발생하는 경우
+        setIsDragging(true);
+      } else {
+        // 스와이프가 끝난 경우
+        setIsDragging(false);
+      }
+
+      // 만약 스와이프 거리가 충분히 크지 않다면 움직임을 취소
+      if (Math.abs(mx) < 10 && !down) {
+        cancel();
+      }
+
+      api.start({ x: down ? mx : 0 });
+    },
+    { axis: "x", filterTaps: true }
+  );
+
   // 썸네일 클릭 시 해당 슬라이드로 이동 후 다시 클릭 시 페이지 이동
-  const handleLinkClick = (e, videoId, index) => {
-    e.preventDefault();
-    if (index === activeThumbIndex) {
-      const path = EndpointManager.generateUrl(ENDPOINTS.VIDEO_DETAIL, { videoId: videoId });
+  const handleLinkClick = (videoId, index) => {
+    // e.preventDefault();
+    if (!isDragging && index === activeThumbIndex) {
+      const path = EndpointManager.generateUrl(ENDPOINTS.VIDEO_DETAIL, { videoId });
       navigate(path);
     }
   };
 
   // 썸네일 클릭 시 해당 슬라이드로 이동 후 다시 클릭 시 페이지 이동
   // 모바일에서는 onTouchEnd 이벤트로 처리
-  const handleLinkTouch = (e, videoId, index) => {
-    e.preventDefault();
-    if (index === activeThumbIndex) {
-      const path = EndpointManager.generateUrl(ENDPOINTS.VIDEO_DETAIL, { videoId: videoId });
-      navigate(path);
-    }
-  };
+  // const handleLinkTouch = (e, videoId, index) => {
+  //   e.preventDefault();
+  //   if (index === activeThumbIndex) {
+  //     const path = EndpointManager.generateUrl(ENDPOINTS.VIDEO_DETAIL, { videoId: videoId });
+  //     navigate(path);
+  //   }
+  // };
 
   if (isEmpty(previewVideo)) {
     return null;
@@ -144,22 +172,25 @@ const SwiperPreview = React.memo(({ screensMA01 }) => {
           <Swiper className="preview-videos" {...thumbSwiperConfig}>
             {previewVideo.map((video, index) => (
               <SwiperSlide className="preview-video-item" key={video.id}>
-                <Link
-                  className="preview-video-link"
-                  to={EndpointManager.generateUrl(ENDPOINTS.VIDEO_DETAIL, { videoId: video.id })}
-                  onClick={(e) => handleLinkClick(e, video.id, index)}
-                  onTouchEnd={(e) => handleLinkTouch(e, video.id, index)}
-                  aria-label={video.title}
-                >
-                  <picture className="preview-thumbnail-wrapper">
-                    <LazyLoadImage
-                      className="preview-thumbnail-image"
-                      src={fBackgroundImage(video.thumbnail, true)}
-                      alt={video.title}
-                      effect="blur"
-                    />
-                  </picture>
-                </Link>
+                <animated.div className="preview-video-link" {...bind(video.id, index)} style={{ x }}>
+                  <Link
+                    to={EndpointManager.generateUrl(ENDPOINTS.VIDEO_DETAIL, { videoId: video.id })}
+                    aria-label={video.title}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      handleLinkClick(video.id, index);
+                    }}
+                  >
+                    <picture className="preview-thumbnail-wrapper">
+                      <LazyLoadImage
+                        className="preview-thumbnail-image"
+                        src={fBackgroundImage(video.thumbnail, true)}
+                        alt={video.title}
+                        effect="blur"
+                      />
+                    </picture>
+                  </Link>
+                </animated.div>
               </SwiperSlide>
             ))}
           </Swiper>
